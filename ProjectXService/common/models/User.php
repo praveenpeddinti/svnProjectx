c4fe2e0617626f95d225ee1433d6c211b3f3918f
MadanGopal <MadanGopal@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-01-23 12:05:50 +0000

    ProjectX Service API - Yii2 Advanced Template project - DB set to 10.10.73.111 (MySql and Mongo)
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/branches@8 b02b8b71-e8a6-4a5c-b3be-45d727391186

---
+++ ProjectXService/common/models/User.php
@@ -0,0 +1,42 @@
+<?php 
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace common\models;
+
+use Yii;
+use yii\base\NotSupportedException;
+use yii\behaviors\TimestampBehavior;
+use yii\db\ActiveRecord;
+use yii\web\IdentityInterface;
+
+class User extends ActiveRecord 
+{
+    
+    public static function tableName()
+    {
+        return '{{%User}}';
+    }
+    
+    public function behaviors()
+    {
+        return [
+            TimestampBehavior::className(),
+        ];
+    }
+    
+    public static function findByUsername($username)
+    {
+        $qry = "select * from User where username='".$username."'";
+        $data = Yii::$app->db->createCommand($qry)->queryAll();
+        return $data;
+//        return static::findOne(['username' => $username, 'status' => self::STATUS_ACTIVE]);
+    }
+    
+}
+
+
+
+?>
---
+++ ProjectXService/environments/dev/common/config/main-local.php
@@ -0,0 +1,25 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=10.10.73.111;dbname=SkiptaCustomGroupsTesting',
+            'username' => 'root',
+            'password' => 'techo2',
+            'charset' => 'utf8',
+        ],
+        'mongodb' => [
+            'class' => 'yii\mongodb\Connection',
+            'dsn' => 'mongodb://10.10.73.111:27017/SkiptaNeo_NEW',
+
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+    ],
+];
