352fb8c56c5dd192899c527cf912e22b48a80c72
moinhussain <moinhussain@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-01-24 12:19:21 +0000

    configuration changes done
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/branches@20 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- ProjectXService/environments/dev/frontend/config/main-local.php
+++ ProjectXService/environments/dev/frontend/config/main-local.php
@@ -4,7 +4,27 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'lEZVlCDuBSjlJM4qzv8W-CZg9ij6lUUG',
+        ],
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=10.10.73.33;dbname=Techo2_ProjectX',
+            'username' => 'root',
+            'password' => 'techo2',
+            'charset' => 'utf8',
+        ],
+        'mongodb' => [
+            'class' => 'yii\mongodb\Connection',
+            'dsn' => 'mongodb://10.10.73.111:27017/SkiptaNeo_NEW',
+
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
         ],
     ],
 ];
