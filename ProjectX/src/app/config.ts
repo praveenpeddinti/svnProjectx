7bf4282920ac00c379c687e99dd514432f386f3e
moinhussain <moinhussain@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-01-24 12:44:31 +0000

    configuration changes done
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/branches@21 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- ProjectX/src/app/config.ts
+++ ProjectX/src/app/config.ts
@@ -1,6 +1,6 @@
 
 export const GlobalVariable = Object.freeze({
     // BASE_API_URL: 'https://otsukaapi.skiptaneo.com/',
-     BASE_API_URL :'http://10.10.73.33/?r='
+     BASE_API_URL :'http://10.10.73.33/'
      //... more of your variables
  });
