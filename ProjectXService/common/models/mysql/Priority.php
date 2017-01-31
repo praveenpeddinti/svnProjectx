1fd4c323f8cc85be2a709e7161a326b28723602b
moinhussain <moinhussain@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-01-31 10:39:36 +0000

    Added new models
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/branches@37 b02b8b71-e8a6-4a5c-b3be-45d727391186

---
+++ ProjectXService/common/models/mysql/Priority.php
@@ -0,0 +1,50 @@
+<?php 
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace common\models\mysql;
+
+use Yii;
+use yii\base\NotSupportedException;
+use yii\behaviors\TimestampBehavior;
+use yii\db\ActiveRecord;
+use yii\web\IdentityInterface;
+
+class Priority extends ActiveRecord 
+{
+    
+    public static function tableName()
+    {
+        return '{{%Priority}}';
+    }
+    
+    public function behaviors()
+    {
+        return [
+            TimestampBehavior::className(),
+        ];
+    }
+    /**
+     * @author Moin Hussain
+     * @param type $priorityId
+     * @return type
+     */
+    public static function getPriorityDetails($priorityId)
+    {
+         try{
+         $query = "select * from Priority where Id=".$priorityId;
+         $data = Yii::$app->db->createCommand($query)->queryOne();
+        return $data;  
+         } catch (Exception $ex) {
+    Yii::log("Priority:getPriorityDetails::" . $ex->getMessage() . "--" . $ex->getTraceAsString(), 'error', 'application');
+         }
+       
+    }
+    
+}
+
+
+
+?>
---
+++ ProjectXService/common/models/mysql/WorkFlowFields.php
@@ -0,0 +1,52 @@
+<?php 
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace common\models\mysql;
+
+use Yii;
+use yii\base\NotSupportedException;
+use yii\behaviors\TimestampBehavior;
+use yii\db\ActiveRecord;
+use yii\web\IdentityInterface;
+
+class WorkFlowFields extends ActiveRecord 
+{
+    
+    public static function tableName()
+    {
+        return '{{%WorkFlowFields}}';
+    }
+    
+    public function behaviors()
+    {
+        return [
+            TimestampBehavior::className(),
+        ];
+    }
+    /**
+     * @author Moin Hussain
+     * @param type $workFlowId
+     * @return type
+     */
+    public static function getWorkFlowDetails($workFlowId)
+    {
+        try{
+        $query = "select Id,Name from WorkFlowFields where Id=".$workFlowId;
+        $data = Yii::$app->db->createCommand($query)->queryOne();
+        return $data;   
+        } catch (Exception $ex) {
+Yii::log("WorkFlowFields:getWorkFlowDetails::" . $ex->getMessage() . "--" . $ex->getTraceAsString(), 'error', 'application');
+
+        }
+       
+//        return static::findOne(['username' => $username, 'status' => self::STATUS_ACTIVE]);
+    }
+    
+}
+
+
+
+?>
