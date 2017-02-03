898f563cbc9f0126fe93c526ff1da86a12efee97
anand6396 <anand6396@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-02-03 12:21:56 +0000

    front end controller
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/branches@67 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- ProjectXService/frontend/service/StoryService.php
+++ ProjectXService/frontend/service/StoryService.php
@@ -1,22 +1,36 @@
 <?php
 namespace frontend\service;
-
+use common\models\mongo\TicketCollection;
 use common\components\CommonUtility;
+use Yii;
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 class StoryService {
+
     /**
      * @author Moin Hussain
      * @param type $ticketId
      * @param type $projectId
      * @return type
      */
-        public function getTicketDetails($ticketId, $projectId) {
+    public function getTicketDetails($ticketId, $projectId) {
         try {
+            $model = new TicketCollection();
+            $ticketDetails = $model->getTicketDetails($ticketId, $projectId);
+
+            $tinyUser = new TinyUserCollection();
+            $assignedToDetails = $tinyUser->getMiniUserDetails($ticketDetails["AssignedTo"]);
+            $reportedByDetails = $tinyUser->getMiniUserDetails($ticketDetails["ReportedBy"]);
+
+            $bucketObject = new Bucket();
+            $bucketName = $bucketObject->getBucketName($ticketDetails["Bucket"], $ticketDetails["ProjectId"]);
+            $ticketTypeObject = new TicketType();
+            $ticketTypeDetails = $ticketTypeObject->getTicketType($ticketDetails["TicketType"]);
          $details =  CommonUtility::prepareTicketDetails($ticketId, $projectId);
          return $details;
         } catch (Exception $ex) {
@@ -26,12 +40,89 @@ class StoryService {
       } 
       public function getAllTicketDetails($projectId) {
         try {
-            
+
+            $priorityObj = new Priority();
+            $priorityDetails = $priorityObj->getPriorityDetails($ticketDetails["Priority"]);
+            $projectObj = new Projects();
+            $projectDetails = $projectObj->getProjectMiniDetails($ticketDetails["ProjectId"]);
+            $workFlowObj = new WorkFlowFields();
+            $workFlowDetails = $workFlowObj->getWorkFlowDetails($ticketDetails["Status"]);
+            $ticketDetails["Priority"] = $priorityDetails;
+            $ticketDetails["Project"] = $projectDetails;
+            $ticketDetails["Status"] = $workFlowDetails;
+            $ticketDetails["AssignedTo"] = $assignedToDetails;
+            $ticketDetails["ReportedBy"] = $reportedByDetails;
+            $ticketDetails["Bucket"] = $bucketName;
+            $ticketDetails["TicketType"] = $ticketTypeDetails;
+            //error_log(print_r($priorityDetails)."-----".print_r($projectDetails)."--".print_r($workFlowDetails)."--".print_r($tinyUserDetails));
+            // error_log(print_r($ticketDetails));
+            return $ticketDetails;
+
          $details =  CommonUtility::prepareTicketDetails($ticketId, $projectId);
          print_r($details);
+
         } catch (Exception $ex) {
             Yii::log("StoryService:getTicketDetails::" . $ex->getMessage() . "--" . $ex->getTraceAsString(), 'error', 'application');
         }
+    }
 
-      }
+    /**
+     * @author Anand Singh
+     * @param type $projectId
+     * @return type
+     */
+    public static function getStoryFields($projectId) {
+        try {
+            $qry = "select * from StoryFields";
+            $data = Yii::$app->db->createCommand($qry)->queryAll();
+            return $data;
+        } catch (Exception $exc) {
+            Yii::log("StoryService:getStoryFields::" . $ex->getMessage() . "--" . $ex->getTraceAsString(), 'error', 'application');
+        }
+    }
+
+    /**
+     * @author Anand Singh
+     * @return type
+     */
+    public static function getPriority() {
+        try {
+            $qry = "select * from Priority";
+            $data = Yii::$app->db->createCommand($qry)->queryAll();
+            return $data;
+        } catch (Exception $exc) {
+            Yii::log("StoryService:getPriority::" . $ex->getMessage() . "--" . $ex->getTraceAsString(), 'error', 'application');
+        }
+    }
+
+    /**
+     * @author Anand Singh
+     * @return type
+     */
+    public static function getPlanLevel() {
+        try {
+            $qry = "select * from PlanLevel";
+            $data = Yii::$app->db->createCommand($qry)->queryAll();
+            return $data;
+        } catch (Exception $exc) {
+            Yii::log("StoryService:getPlanLevel::" . $ex->getMessage() . "--" . $ex->getTraceAsString(), 'error', 'application');
+        }
+    }
+
+    /**
+     * @author Anand Singh
+     * @return type
+     */
+    public static function getTicketType() {
+        try {
+            $qry = "select * from TicketType";
+            $data = Yii::$app->db->createCommand($qry)->queryAll();
+            return $data;
+        } catch (Exception $exc) {
+            Yii::log("StoryService:getTicketType::" . $ex->getMessage() . "--" . $ex->getTraceAsString(), 'error', 'application');
+        }
+    }
+
 }
+
+  
