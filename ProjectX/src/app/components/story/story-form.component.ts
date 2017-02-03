b677d6eb8020335759e32adb10405c1bc59e0770
anand6396 <anand6396@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-02-03 11:52:31 +0000

    forgot pass
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/branches@62 b02b8b71-e8a6-4a5c-b3be-45d727391186

---
+++ ProjectX/src/app/components/story/story-form.component.ts
@@ -0,0 +1,64 @@
+import { Component } from '@angular/core';
+import { StoryService} from '../../services/story.service';
+
+@Component({
+    selector: 'story-form',
+    templateUrl: 'story-form.html',
+    providers: [StoryService]     
+    	
+})
+
+export class StoryComponent {
+    public test="Anand";
+    public storyFormData=[];
+    public storyData={};
+    public form={};
+    constructor( private _service: StoryService) { }
+
+    ngOnInit() {
+     
+        this._service.getStoryFields(1,(response)=>{
+              let jsonForm={};
+              let DefaultValue;
+             // console.log("new_data"+JSON.stringify(response));
+              if(response.statusCode==200){
+                  response.data.story_fields.forEach(element => {
+                    var  item = element.Field_Name;
+                    
+                    if(element.Type == 5){
+                        element.DefaultValue=new Date().toLocaleString();
+                    }else if(element.Type == 6){
+                        DefaultValue=response.data.collaborators;
+                    }else if(element.Type == 2){
+                       if(item=='Priority'){
+                             DefaultValue=response.data.priority;
+                        }else if(item=='PlanLevel'){
+                            DefaultValue=response.data.plane_level;
+                        }else if(item=='Type'){
+                           DefaultValue=response.data.ticket_type;
+                        }else if(item=='Status'){
+                           DefaultValue=response.data.status;
+                        }
+                    } 
+                    
+                    console.log("Default values"+JSON.stringify(DefaultValue));
+                    jsonForm[item] = element.DefaultValue;
+                   this.storyFormData.push(
+                       {'lable':element.Title,'model':element.Field_Name,'value':element.DefaultValue,'required':element.Required,'readOnly':element.ReadOnly,'type':element.Type,'values':DefaultValue}
+                       )
+                  });
+                this.form = jsonForm;
+                  console.log(JSON.stringify(this.form ));
+            }else{
+            console.log("fail---");
+            }
+        });
+    }
+saveStory(){
+    this._service.saveStory(this.form,(response)=>{
+
+
+
+    });
+}
+}
