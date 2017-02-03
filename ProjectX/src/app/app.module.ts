8f83997aa0a32fc93696e8ef18803ebf15484640
anand6396 <anand6396@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-02-03 11:52:03 +0000

    forgot pass
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/branches@61 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- ProjectX/src/app/app.module.ts
+++ ProjectX/src/app/app.module.ts
@@ -7,6 +7,12 @@ import { AppComponent }  from './app.component';
 import { LoginComponent }  from './components/login/login.component';
 import { HomeComponent }  from './components/home/home.component';
 import { Ng2DropdownModule } from 'ng2-material-dropdown';
+import { DatePickerModule } from 'ng2-datepicker';
+import { Typeahead } from 'ng2-typeahead';
+import { MentionModule } from 'angular2-mentions/mention';
+import {StoryService} from './services/story.service';
+import { CKEditorModule } from 'ng2-ckeditor';
+import {Ng2DragDropModule} from "ng2-drag-drop";
 // HashLocationStrategy added to avoid Refresh Problems on Web Server....
 import {LocationStrategy, HashLocationStrategy} from '@angular/common';
 import {LoginService, User} from './services/login.service';
@@ -15,7 +21,7 @@ import {FlexLayoutModule} from '@angular/flex-layout';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import {AuthGuard} from './services/auth-guard.service';
-
+import { StoryComponent }  from './components/story/story-form.component';
 const ROUTES=[
               {path: '',redirectTo: 'login',pathMatch: 'full' },
               {path: 'home',children:[
@@ -26,12 +32,22 @@ const ROUTES=[
               {path: 'login', component: LoginComponent},
              ];
 @NgModule({
-  imports:      [ FlexLayoutModule,BrowserModule ,FormsModule,ReactiveFormsModule ,HttpModule,Ng2DropdownModule,
-  RouterModule.forRoot(ROUTES)
+  imports:      [
+   BrowserModule,
+   FormsModule,
+   ReactiveFormsModule ,
+   HttpModule,
+   Ng2DropdownModule,
+   DatePickerModule,
+   MentionModule,
+   CKEditorModule,
+   Ng2DragDropModule,
+   RouterModule.forRoot(ROUTES)
   ],
-  declarations: [ AppComponent,LoginComponent,HomeComponent, HeaderComponent,FooterComponent ],
+
+  declarations: [ AppComponent,LoginComponent,HomeComponent, HeaderComponent,FooterComponent,StoryComponent ],
   bootstrap:    [ AppComponent ],
-  providers:[LoginService,AjaxService,AuthGuard,{provide: LocationStrategy, useClass: HashLocationStrategy}],
+  providers:[LoginService,AjaxService,AuthGuard,{provide: LocationStrategy, useClass: HashLocationStrategy},StoryService],
   schemas: [ CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule { }
