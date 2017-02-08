f38c0733cfa055cf6e28449e065be323ef40fd38
hareesh.bekkam <hareesh.bekkam@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-02-08 07:43:51 +0000

    File upload
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/branches@94 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- ProjectX/src/app/components/story/story-form.component.ts
+++ ProjectX/src/app/components/story/story-form.component.ts
@@ -16,7 +16,14 @@ export class StoryComponent {
     public toolbar={toolbar : [
     [ 'Heading 1', '-', 'Bold','-', 'Italic','-','Underline','Link','NumberedList','BulletedList' ]
 ]};
-    constructor( private _service: StoryService) { }
+filesToUpload: Array<File>;
+//sampleModel:string = "";
+public hasBaseDropZoneOver:boolean = false;
+public hasFileDroped:boolean = false;
+
+    constructor( private _service: StoryService) {
+        this.filesToUpload = [];
+     }
 
     ngOnInit() {
      
@@ -48,6 +55,63 @@ export class StoryComponent {
             }
         });
     }
+
+public fileOverBase(e:any):void {
+    this.hasBaseDropZoneOver = e;
+}
+
+public fileChangeEvent(fileInput: any):void {
+  this.filesToUpload = <Array<File>> fileInput.target.files;
+    this.hasBaseDropZoneOver = false;
+        this.makeFileRequest("http://10.10.73.62:4200/upload", [], this.filesToUpload).then((result :Array<any>) => {
+            for(var i = 0; i<result.length; i++){
+                //this.sampleModel = this.sampleModel + "[[file:" +result[i].path + "]] ";
+                this.form['description'] = this.form['description'] + "[[file:" +result[i].path + "]] ";
+            }
+        }, (error) => {
+            console.error(error);
+            //this.sampleModel = "Error while uploading";
+            this.form['description'] = "Error while uploading";
+        });
+}
+public onFileDrop(fileInput:any): void{
+    //this.hasFileDroped = fileInput;
+    this.filesToUpload = <Array<File>> fileInput.dataTransfer.files;
+
+      this.makeFileRequest("http://10.10.73.62:4200/upload", [], this.filesToUpload).then((result :Array<any>) => {
+            for(var i = 0; i<result.length; i++){
+                //this.sampleModel = this.sampleModel + "[[file:" +result[i].path + "]] ";
+                this.form['description'] = this.form['description'] + "[[file:" +result[i].path + "]] ";
+            }
+        }, (error) => {
+            console.error(error);
+            //this.sampleModel = "Error while uploading";
+            this.form['description'] = "Error while uploading";
+        });
+  }
+
+public makeFileRequest(url: string, params: Array<string>, files: Array<File>) {
+        return new Promise((resolve, reject) => {
+            var formData: any = new FormData();
+            var xhr = new XMLHttpRequest();
+            for(var i = 0; i < files.length; i++) {
+                formData.append("uploads[]", files[i], files[i].name);
+            }
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 200) {
+                        //console.log("the responc " + JSON.parse(xhr.response))
+                        resolve(JSON.parse(xhr.response));
+                    } else {
+                        reject(xhr.response);
+                    }
+                }
+            }
+            xhr.open("POST", url, true);
+            xhr.send(formData);
+        });
+    }
+
 saveStory(){
     console.log("post____data"+JSON.stringify(this.form));
     this._service.saveStory(this.form,(response)=>{
