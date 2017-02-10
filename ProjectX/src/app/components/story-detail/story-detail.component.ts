d8c3abf1c92b14fe0acfa0a43020acf891ac411f
MadanGopal <MadanGopal@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-02-10 07:28:19 +0000

    Changes in Get field details service. Added ajax request response.
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/branches@140 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- ProjectX/src/app/components/story-detail/story-detail.component.ts
+++ ProjectX/src/app/components/story-detail/story-detail.component.ts
@@ -3,10 +3,12 @@ import { Router } from '@angular/router';
 import { Headers, Http } from '@angular/http';
 import { AjaxService } from '../../ajax/ajax.service';
 
+
 @Component({
   selector: 'app-story-detail',
   templateUrl: './story-detail.component.html',
-  styleUrls: ['./story-detail.component.css']
+  styleUrls: ['./story-detail.component.css'],
+  
 })
 export class StoryDetailComponent implements OnInit {
   // private _ajaxService: AjaxService;
@@ -18,7 +20,13 @@ export class StoryDetailComponent implements OnInit {
 
   private ticketData;
   private fieldsData = [];
-  private showMyEditableField =[]; 
+  private showMyEditableField =[];
+  private ticketDesc="";
+  private toolbarForDetail={toolbar : [
+    [ 'Heading 1', '-', 'Bold','-', 'Italic','-','Underline','Link','NumberedList','BulletedList' ]
+]};
+  // private makeFocused = []; 
+  private dropList=[];
   constructor( private _ajaxService: AjaxService,
     public _router: Router,
     private http: Http) { 
@@ -29,6 +37,7 @@ export class StoryDetailComponent implements OnInit {
          
          this.ticketData = data;
          alert("++++++++++++++"+JSON.stringify(this.ticketData));
+         this.ticketDesc = data.data.Description;
          this.fieldsData = this.fieldsDataBuilder(data.data.Fields,data.data.TicketId);
         //  alert("========>"+JSON.stringify(this.fieldsData));
          
@@ -44,7 +53,7 @@ export class StoryDetailComponent implements OnInit {
     alert(val);
   }
 
-  editThisField(event,fieldIndex){
+  editThisField(event,fieldIndex,fieldId,fieldDataId){
     console.log(event.target.id);
     // var thisFieldId = event.target.id;
     // var thisField;
@@ -56,8 +65,38 @@ export class StoryDetailComponent implements OnInit {
     //     replaceHtml.setAttribute("id",thisFieldId);//"<input type='text' value='"+thisField.textContent+"'/>"
     //     thisField.parentNode.replaceChild(replaceHtml,thisField);
     // }
-    
+    this.dropList=[];
+    var fieldName = fieldId.split("_")[1];
+    var inptFldId = fieldId+"_"+fieldIndex;
     this.showMyEditableField[fieldIndex] = false;
+    setTimeout(()=>{document.getElementById(inptFldId).focus();},150);
+var reqData = {
+  FieldId:fieldDataId,
+  ProjectId:this.ticketData.data.Project.PId,
+  TicketId:this.ticketData.data.TicketId
+};
+// alert(JSON.stringify(reqData)+"reqest data");
+this._ajaxService.AjaxSubscribe("story/get-field-details-by-field-id",reqData,(data)=>
+    { 
+       
+         console.log(JSON.stringify(data));
+         this.dropList = data.getFieldDetails;
+        
+         
+    });
+
+    switch(fieldName){
+      case "planlevel":
+      this.dropList=["Level1","Level2","Level3","Final Level"];
+      break;
+      case "tickettype":
+      this.dropList=["Story","Task","Time Pass","Trash","Not yet Decided"];
+      break;
+      case "stakeholder":
+      this.dropList=["Madan","Jagadish","Bekkam","Kishore","Moin","Paddamma"];
+      break;
+    }
+    
 
   }
 
@@ -77,10 +116,10 @@ export class StoryDetailComponent implements OnInit {
     }
   fieldsDataBuilder(fieldsArray,ticketId){
     let fieldsBuilt = [];
-    let data = {title:"",value:"",readonly:true,required:true,id:"",fieldType:"",renderType:"",type:""};
+    let data = {title:"",value:"",valueId:"",readonly:true,required:true,elId:"",fieldType:"",renderType:"",type:"",Id:""};
     for(let field of fieldsArray){
       if(field.field_name != "customfield_2"){
-      data = {title:"",value:"",readonly:true,required:true,id:"",fieldType:"",renderType:"",type:""};
+      data = {title:"",value:"",valueId:"",readonly:true,required:true,elId:"",fieldType:"",renderType:"",type:"",Id:""};
           switch(field.field_type){
             case "Text":
             data.title = field.title;
@@ -91,6 +130,7 @@ export class StoryDetailComponent implements OnInit {
             case "List":
             data.title = field.title;
             data.value = field.readable_value.Name;
+            data.valueId = field.readable_value.Id
             data.renderType = "select";
             break;
             case "Numeric":
@@ -102,20 +142,21 @@ export class StoryDetailComponent implements OnInit {
             case "Date":
             data.title = field.title;
             // alert(field.readable_value.date.split(" ")[0]+"++++++++Date++++++++++");
-            data.value = field.readable_value.date.split(" ")[0];
+            data.value = field.readable_value;
             data.renderType = "input";
             data.type="date";
             break;
             case "DateTime":
             data.title = field.title;
             // alert(field.readable_value.date.split(".")[0]+"++++++++++DateTime++++++++++++");
-            data.value = field.readable_value.date.split(".")[0];
+            data.value = field.readable_value;
             data.renderType = "input";
             data.type="datetime";
             break;
             case "Team List":
             data.title = field.title;
             data.value = field.readable_value.UserName;
+            data.valueId = field.readable_value.CollaboratorId
             data.renderType = "select";
             break;
             // case "Checkbox":
@@ -123,13 +164,15 @@ export class StoryDetailComponent implements OnInit {
             case "Bucket":
             data.title = field.title;
             data.value = field.readable_value.Name;
+            data.valueId = field.readable_value.Id
             data.renderType = "select";
             break;
 
           }
           data.readonly = (field.readonly == 1)?true:false;
           data.required = (field.required == 1)?true:false;
-          data.id =  ticketId+"_"+field.field_name;
+          data.elId =  ticketId+"_"+field.field_name;
+          data.Id = field.Id;
           // if(field.field_type == "Bucket" || field.field_type == "Team List" || field.field_type == "Team List"){
           //   data.type = "List";
           // }else{
@@ -137,6 +180,7 @@ export class StoryDetailComponent implements OnInit {
           // }
           fieldsBuilt.push(data);
           this.showMyEditableField.push((field.readonly == 1)?false:true);
+          // this.makeFocused.push(new EventEmitter());
       }
     }
 
