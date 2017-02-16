457d66d1332ff638ac77c3e2c0947957b60406ad
kishore.neelam@techo2 <kishore.neelam@techo2@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-02-16 08:59:20 +0000

    added styles for the custom component and the Loading
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/branches@231 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- ProjectX/src/app/ajax/ajax.service.ts
+++ ProjectX/src/app/ajax/ajax.service.ts
@@ -4,6 +4,8 @@ import { Headers, Http } from '@angular/http';
 import { GlobalVariable } from '../../app/config';
 
 var headers = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' });
+
+declare var jQuery:any;
 //headers.append('Authorization',"sdssqweqw2111");
  var  Url = GlobalVariable.BASE_API_URL;  // URL to web api
 @Injectable()
@@ -13,14 +15,19 @@ export class AjaxService {
 
 AjaxSubscribe(url:string,params:Object,callback)
 {   
-  
+   jQuery("#commonSpinner").removeClass("unloading"); 
+   jQuery("#commonSpinner").addClass("loading"); 
    var getAllData=  JSON.parse(localStorage.getItem('user'));
-   params["userInfo"] = getAllData;
-   params["projectId"] = 1;
+    if(getAllData != null){
+      params["userInfo"] = getAllData;
+      params["projectId"] = 1;
+    }
       //var  options = new RequestOptions({headers: headers});
       this.http.post(GlobalVariable.BASE_API_URL+url, JSON.stringify(params), headers)
       .subscribe(
       (data) => {
+        jQuery("#commonSpinner").removeClass("loading"); 
+        jQuery("#commonSpinner").addClass("unloading"); 
         var res = data.json();//For Success Response
           callback(res);
       },
