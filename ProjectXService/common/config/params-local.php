0c92c2eef92c1b4c9cf8481fa564cd9aab161a7e
moinhussain <moinhussain@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-02-17 12:03:12 +0000

    commited edit ticket saving
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/branches@265 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- ProjectXService/common/config/params-local.php
+++ ProjectXService/common/config/params-local.php
@@ -1,4 +1,7 @@
 <?php
 return [
     "ServerURL" => "http://10.10.73.33",
+    "ProjectRoot" =>"/usr/share/nginx/www/ProjectXService/frontend/web",
+    "StoryArtifactPath" => "/files/story",
+    
 ];
