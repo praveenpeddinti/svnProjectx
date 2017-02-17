bb85dce0a04a549902b7e0a70ab32795b75f1bcb
hareesh.bekkam <hareesh.bekkam@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-02-17 07:41:26 +0000

    File Upload common functionality
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/branches@254 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- ProjectX/src/app/config.ts
+++ ProjectX/src/app/config.ts
@@ -3,7 +3,7 @@ export const GlobalVariable = Object.freeze({
     // BASE_API_URL: 'https://otsukaapi.skiptaneo.com/',
      //BASE_API_URL :'http://10.10.73.33/'
      BASE_API_URL :'https://10.10.73.62/',
-     FILE_UPLOAD_URL: 'http://10.10.73.62:4200/upload'
+     FILE_UPLOAD_URL: 'http://10.10.73.33:4201/upload'
 
      //... more of your variables
  });
