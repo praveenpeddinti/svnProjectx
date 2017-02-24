82f67454b83375a14e4dc441b6258a3eaf58fe23
suryaprakash.pendyala <suryaprakash.pendyala@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-02-24 13:41:20 +0000

    Code formating and comments added
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/branches@331 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- ProjectX/src/app/components/story-edit/story-edit.component.ts
+++ ProjectX/src/app/components/story-edit/story-edit.component.ts
@@ -8,8 +8,8 @@ import {CalendarModule} from 'primeng/primeng';
 import { FileUploadService } from '../../services/file-upload.service';
 import { GlobalVariable } from '../../config';
 
+declare var jQuery:any; //reference to JQUERY  
 
-declare var jQuery:any;
 @Component({
   selector: 'app-story-edit',
   templateUrl: './story-edit.component.html',
@@ -17,27 +17,26 @@ declare var jQuery:any;
   providers:[StoryService]
 })
 
-export class StoryEditComponent implements OnInit {
-
-public dragTimeout;
-public minDate:Date;
-
-    private ticketData:any=[];
-    private ticketid;
-    private url_TicketId;
-    private description;
-    public form={};
+export class StoryEditComponent implements OnInit 
+{
+  public dragTimeout;
+  public minDate:Date;
+  private ticketData:any=[];
+  private ticketid;
+  private url_TicketId;
+  private description;
+  public form={};
   private fieldsData = [];
-  // private fieldsBindingArray=[];
   private showMyEditableField =[];
   public dragdrop={extraPlugins:'dragdrop'};
+   //CkEditor Configuration Options
   public toolbar={toolbar : [
-    [ 'Heading 1', '-', 'Bold','-', 'Italic','-','Underline','Link','NumberedList','BulletedList']
-],removePlugins:'elementspath',resize_enabled:true};
-public filesToUpload: Array<File>;
-public hasBaseDropZoneOver:boolean = false;
-public hasFileDroped:boolean = false;
-public fileUploadStatus:boolean = false;
+      [ 'Heading 1', '-', 'Bold','-', 'Italic','-','Underline','Link','NumberedList','BulletedList']
+  ],removePlugins:'elementspath',resize_enabled:true};
+  public filesToUpload: Array<File>;
+  public hasBaseDropZoneOver:boolean = false;
+  public hasFileDroped:boolean = false;
+  public fileUploadStatus:boolean = false;
 
   constructor(private fileUploadService: FileUploadService, private _ajaxService: AjaxService,private _service: StoryService,
     public _router: Router,
@@ -46,55 +45,42 @@ public fileUploadStatus:boolean = false;
     }
 
 
-  ngOnInit() {
+  ngOnInit() 
+  {
+    //get route param(TicketId) for specific ticket to edit
     this.route.params.subscribe(params => {
             this.url_TicketId = params['id'];
         });
-setTimeout(()=>{
-  this._ajaxService.AjaxSubscribe("story/edit-ticket",{ticketId:this.url_TicketId},(data)=>
-    { 
-       
-         
-         this.ticketData = data.data;
-         this.description=data.data.CrudeDescription;
-         //console.log("++++++++++++++--"+data.data.CrudeDescription);
-         this.fieldsData = this.fieldsDataBuilder(data.data.Fields,data.data.TicketId);
-        //  var t = this.fieldsData.length;
-        //  this.fieldsBindingArray[t];
-        // console.log("Field Data----"+this.fieldsData);
-         
-    });
 
-},150);
+    //setTimeout(()=>{
+      this._ajaxService.AjaxSubscribe("story/edit-ticket",{ticketId:this.url_TicketId},(data)=>
+        {   
+             this.ticketData = data.data;
+             this.description=data.data.CrudeDescription;
+             this.fieldsData = this.fieldsDataBuilder(data.data.Fields,data.data.TicketId);        
+        });
     
-    this.minDate=new Date();
+    //},150);
+    
+    this.minDate=new Date(); //set the min date to current date
   }
 
-   editThisField(event,fieldIndex){
-   // console.log(event.target.id);
-    // var thisFieldId = event.target.id;
-    // var thisField;
-    // var replaceHtml ;
-    // if(thisFieldId !="" && thisFieldId !=null){
-    //     thisField = document.getElementById(thisFieldId);
-    //     replaceHtml = document.createElement("input");
-    //     replaceHtml.setAttribute("value",thisField.textContent);
-    //     replaceHtml.setAttribute("id",thisFieldId);//"<input type='text' value='"+thisField.textContent+"'/>"
-    //     thisField.parentNode.replaceChild(replaceHtml,thisField);
-    // }
-    
-    this.showMyEditableField[fieldIndex] = false;
-
-  }
-
-
-  fieldsDataBuilder(fieldsArray,ticketId){
+  /*
+    @params    :  fieldsArray,ticketId
+    @ParamType :  array,int
+    @Description: Dynamic Ticket/Story Fields Rendering
+    */
+  fieldsDataBuilder(fieldsArray,ticketId)
+  {
     let fieldsBuilt = [];
     let data = {title:"",value:"",readonly:true,required:true,id:"",fieldDataId:"",fieldName:"",fieldType:"",renderType:"",type:"",listdata:[]};
-    for(let field of fieldsArray){
-      if(field.field_name != "customfield_2"){
-      data = {title:"",value:"",readonly:true,required:true,id:"",fieldDataId:"",fieldName:"",fieldType:"",renderType:"",type:"",listdata:[]};
-          switch(field.field_type){
+    for(let field of fieldsArray)
+    {
+      if(field.field_name != "customfield_2")
+      {
+          data = {title:"",value:"",readonly:true,required:true,id:"",fieldDataId:"",fieldName:"",fieldType:"",renderType:"",type:"",listdata:[]};
+          switch(field.field_type)
+          {
             case "Text":
             data.title = field.title;
             data.value = field.value;
@@ -116,14 +102,12 @@ setTimeout(()=>{
             break;
             case "Date":
             data.title = field.title;
-            // alert(field.readable_value.date.split(" ")[0]+"++++++++Date++++++++++");
             data.value = field.readable_value;
             data.renderType = "date";
             data.type="date";
             break;
             case "DateTime":
             data.title = field.title;
-            // alert(field.readable_value.date.split(".")[0]+"++++++++++DateTime++++++++++++");
             data.value = field.readable_value;
             data.renderType = "date";
             data.type="datetime";
@@ -134,10 +118,7 @@ setTimeout(()=>{
             data.renderType = "select";
             data.listdata = this.ticketData.collaborators;
             data.fieldDataId = field.readable_value.CollaboratorId;
-            
             break;
-            // case "Checkbox":
-            // break;
             case "Bucket":
             data.title = field.title;
             data.value = field.readable_value.Name;
@@ -145,48 +126,58 @@ setTimeout(()=>{
             data.listdata = field.meta_data;
             data.fieldDataId = field.readable_value.Id;
             break;
-
           }
+
           data.readonly = (field.readonly == 1)?true:false;
           data.required = (field.required == 1)?true:false;
           data.id =  ticketId+"_"+field.field_name;
-          // if(field.field_type == "Bucket" || field.field_type == "Team List" || field.field_type == "Team List"){
-          //   data.type = "List";
-          // }else{
-            data.fieldType = field.field_type;
-          // }
+          data.fieldType = field.field_type;
           data.fieldName =  field.Id;
-          //console.log(field.field_name);
-          if(field.field_name == "dod"){
+          if(field.field_name == "dod")
+          {
               data.renderType = "textarea";
-             // console.log(data.renderType);
           }
           var priority=(data.title=="Priority"?true:false);
           var status=data.title;
           data.listdata=this.prepareItemArray(data.listdata,priority,status);
           fieldsBuilt.push(data);
           this.showMyEditableField.push((field.readonly == 1)?false:true);
-          // this.fieldsBindingArray.push(field.Id);
       }
     }
-//console.log(JSON.stringify(fieldsBuilt));
+
     return fieldsBuilt;
 
   }
- public prepareItemArray(list:any,priority:boolean,status){
-  var listItem=[];
-     if(list.length>0){ 
-       if(status == "Assigned to" || status == "Stake Holder"){
-       listItem.push({label:"--Select a Member--", value:"",priority:priority,type:status});
+
+  /*
+    @params    :  list,priority,status
+    @ParamType :  any,boolean,string
+    @Description: Building Dynamic Dropdown List Values
+    */
+ public prepareItemArray(list:any,priority:boolean,status)
+ {
+     var listItem=[];
+     if(list.length>0)
+     { 
+       if(status == "Assigned to" || status == "Stake Holder")
+       {
+         listItem.push({label:"--Select a Member--", value:"",priority:priority,type:status});
        }        
-         for(var i=0;list.length>i;i++){
+       for(var i=0;list.length>i;i++)
+       {
           listItem.push({label:list[i].Name, value:list[i].Id,priority:priority,type:status});
        }
      }
-return listItem;
-}
-  editStory(edit_data){
-     // console.log("===Edit Data==="+JSON.stringify(edit_data));
+    return listItem;
+ }
+
+  /*
+    @params    :  edit_data
+    @ParamType :  Object
+    @Description: Submit Edit Story/Ticket
+    */
+  editStorySubmit(edit_data)
+  {  
     jQuery("#title_error").hide();
     jQuery("#desc_error").hide();
     if(edit_data.title=='')
@@ -199,68 +190,87 @@ return listItem;
     }
     if(edit_data.description!="" && edit_data.title!="")
     {
-   var post_data={
-      'data':edit_data,
-     
+       var post_data={
+          'data':edit_data,
+         
+        }
+        this._ajaxService.AjaxSubscribe("story/edit-ticket-details",post_data,(data)=>
+    
+        { 
+         this._router.navigate(['story-detail',this.url_TicketId]);
+        });
     }
-    this._ajaxService.AjaxSubscribe("story/edit-ticket-details",post_data,(data)=>
+          
+   }
 
-    { 
-     this._router.navigate(['story-detail',this.url_TicketId]);
-    });
-    }
-     
-     
-}
-
-checkEmpty(event,id)
-{
-  if(event!="" || event!=null)
+  /*
+    @params    :  event,id
+    @ParamType :  eventobj,string
+    @Description: Check Empty Title or Description
+    */
+  checkEmpty(event,id)
   {
-    var idAttr = id;
-    if(idAttr=='title')
+    if(event!="" || event!=null)
     {
-      jQuery("#title_error").hide();
-    }
-    if(idAttr=='description')
-    {
-      jQuery("#desc_error").hide();
+      var idAttr = id;
+      if(idAttr=='title')
+      {
+        jQuery("#title_error").hide();
+      }
+      if(idAttr=='description')
+      {
+        jQuery("#desc_error").hide();
+      }
     }
   }
-}
 
-public fileOverBase(fileInput:any):void {
-//console.log("fileoverbase");
-    this.hasBaseDropZoneOver = true;
-    if(this.dragTimeout != undefined && this.dragTimeout != "undefined"){ console.log("clear---");
-    clearTimeout(this.dragTimeout);
-    }
+    /*
+    @params      : fileInput
+    @ParamType   :  any
+    @Description : Enabling the dropzone DIV on dragOver
+    */
+  public fileOverBase(fileInput:any):void 
+  {
+      this.hasBaseDropZoneOver = true;
+      if(this.dragTimeout != undefined && this.dragTimeout != "undefined"){ console.log("clear---");
+      clearTimeout(this.dragTimeout);
+      }
+  
+  }
 
-}
+   /*
+    @params      : fileInput
+    @ParamType   :  any
+    @Description : Disabling the dropzone DIV on dragOver
+    */
+  public fileDragLeave(fileInput: any){
+  //console.log("fileDragLeave");
+  var thisObj = this;
+      if(this.dragTimeout != undefined && this.dragTimeout != "undefined"){
+          // console.log("clear---");
+      clearTimeout(this.dragTimeout);
+      }
+       this.dragTimeout = setTimeout(function(){
+       thisObj.hasBaseDropZoneOver = false;
+      },500);
+      
+  }
 
-public fileDragLeave(fileInput: any){
-//console.log("fileDragLeave");
-var thisObj = this;
-    if(this.dragTimeout != undefined && this.dragTimeout != "undefined"){
-        // console.log("clear---");
-    clearTimeout(this.dragTimeout);
-    }
-     this.dragTimeout = setTimeout(function(){
-     thisObj.hasBaseDropZoneOver = false;
-    },500);
-    
-}
-
-public fileUploadEvent(fileInput: any, comeFrom: string):void {
-   //console.log("the source " + comeFrom);
-   // console.log("cahnge event " + fileInput.name +"------- " + fileInput.size);
-   if(comeFrom == 'fileChange'){
-        this.filesToUpload = <Array<File>> fileInput.target.files;
-   } else if(comeFrom == 'fileDrop'){
-        this.filesToUpload = <Array<File>> fileInput.dataTransfer.files;
-   } else{
-        this.filesToUpload = <Array<File>> fileInput.target.files;
-   }
+    /*
+    @params       : fileInput,comeFrom
+    @ParamType    :  any,string
+    @Description  : Uploading File
+    */
+  public fileUploadEvent(fileInput: any, comeFrom: string):void 
+  {
+     
+     if(comeFrom == 'fileChange'){
+          this.filesToUpload = <Array<File>> fileInput.target.files;
+     } else if(comeFrom == 'fileDrop'){
+          this.filesToUpload = <Array<File>> fileInput.dataTransfer.files;
+     } else{
+          this.filesToUpload = <Array<File>> fileInput.target.files;
+     }
 
         this.hasBaseDropZoneOver = false;
         this.fileUploadStatus = true;
@@ -275,15 +285,17 @@ public fileUploadEvent(fileInput: any, comeFrom: string):void {
             }
             this.fileUploadStatus = false;
         }, (error) => {
-            console.error(error);
             this.description = this.description + "Error while uploading";
             this.fileUploadStatus = false;
         });
-}
-    cancelDesc(){
-  // this.ticketEditableDesc = this.ticketCrudeDesc;
-  // this.showDescEditor = true;
-  this._router.navigate(['story-detail',this.url_TicketId]);
-}
+   }
+
+     /*
+     @Description  : Routing to Story Detail on Cancel Edit
+     */
+    cancelDesc()
+    {
+      this._router.navigate(['story-detail',this.url_TicketId]);
+    }
 
 }
