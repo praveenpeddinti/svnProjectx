8e266c3fffe4482e8c2c90d706d76ff044c1b261
suryaprakash.pendyala <suryaprakash.pendyala@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-02-24 12:59:19 +0000

    Code formating and comments added
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/branches@330 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- ProjectX/src/app/components/story/story-form.component.ts
+++ ProjectX/src/app/components/story/story-form.component.ts
@@ -1,14 +1,13 @@
 import { Component,ViewChild,Output } from '@angular/core';
 import { StoryService} from '../../services/story.service';
 import { NgForm } from '@angular/forms';
-import { TinyMCE } from '../../tinymce.component';
 import {Router} from '@angular/router';
 import { FileUploadService } from '../../services/file-upload.service';
 import { GlobalVariable } from '../../config';
-
 import {AccordionModule,DropdownModule,SelectItem,CalendarModule} from 'primeng/primeng';
 
-declare var jQuery:any;     
+declare var jQuery:any;    //Reference to Jquery
+
  @Component({
     selector: 'story-form',
     templateUrl: 'story-form.html',
@@ -17,40 +16,42 @@ declare var jQuery:any;
 
 })
 
-export class StoryComponent {
-@Output() public options = {
-    readAs: 'ArrayBuffer'
-  };
- public dragTimeout;
+export class StoryComponent 
+{
+    @Output() public options = {
+        readAs: 'ArrayBuffer'
+      };
+    public dragTimeout;
     public storyFormData=[];
     public storyData={};
     public form={};
+    //CkEditor Configuration Options
     public toolbar={toolbar : [
     [ 'Heading 1', '-', 'Bold','-', 'Italic','-','Underline','Link','NumberedList','BulletedList' ]
-],removePlugins:'elementspath',resize_enabled:true};
-public filesToUpload: Array<File>;
-//sampleModel:string = "";
-public hasBaseDropZoneOver:boolean = false;
-public hasFileDroped:boolean = false;
-editorData:string='';
-public fileUploadStatus:boolean = false;
+    ],removePlugins:'elementspath',resize_enabled:true};
+    public filesToUpload: Array<File>;
+    public hasBaseDropZoneOver:boolean = false;
+    public hasFileDroped:boolean = false;
+    editorData:string='';
+    public fileUploadStatus:boolean = false;
 
     constructor(private fileUploadService: FileUploadService, private _service: StoryService, private _router:Router) {
         this.filesToUpload = [];
     }
+
   
-    ngOnInit() {
-     
-        this._service.getStoryFields(1,(response)=>{
+    ngOnInit() 
+    {
+        this._service.getStoryFields(1,(response)=>
+        {
               let jsonForm={};
               let DefaultValue;
                jsonForm['title'] ='';
                jsonForm['description'] ='';
-             // console.log("new_data"+JSON.stringify(response));
-              if(response.statusCode==200){
+              if(response.statusCode==200)
+              {
                   response.data.story_fields.forEach(element => {
                     var  item = element.Id;
-                    
                     if(element.Type == 5){
                         element.DefaultValue=new Date().toLocaleString();
                     }else if(element.Type == 6){
@@ -58,87 +59,121 @@ public fileUploadStatus:boolean = false;
                     }else if(element.Type == 2){
                         DefaultValue=element.data; 
                     }           
-                    console.log("Default values"+JSON.stringify(DefaultValue));
                     jsonForm[item] = element.DefaultValue;
                     var priority=(element.Title=="Priority"?true:false);
                     var listItemArray=this.prepareItemArray(DefaultValue,priority,element.Title);
-                   this.storyFormData.push(
+                    this.storyFormData.push(
                        {'lable':element.Title,'model':element.Id,'value':element.DefaultValue,'required':element.Required,'readOnly':element.ReadOnly,'type':element.Type,'values':listItemArray}
                        )
                   });
                 this.form = jsonForm;
-                  console.log(JSON.stringify(this.form ));
-            }else{
-            console.log("fail---");
-            }
+              }else{
+                    console.log("storyFrom Component ngOnInit fail---");
+              }
         });
     }
-public prepareItemArray(list:any,priority:boolean,status:string){
-  var listItem=[];
-     if(list.length>0){
-         for(var i=0;list.length>i;i++){
-          listItem.push({label:list[i].Name, value:list[i].Id,priority:priority,type:status});
-       }
-     }
-return listItem;
-}
-public fileOverBase(fileInput:any):void {
-//console.log("fileoverbase");
-    this.hasBaseDropZoneOver = true;
-    if(this.dragTimeout != undefined && this.dragTimeout != "undefined"){ console.log("clear---");
-    clearTimeout(this.dragTimeout);
+
+    /*
+    @params    :  list,priority,status
+    @ParamType :  any,boolean,string
+    @Description: Preparing DropDown List.
+    */
+    public prepareItemArray(list:any,priority:boolean,status:string)
+    {
+      var listItem=[];
+      if(list.length>0)
+      {
+        for(var i=0;list.length>i;i++)
+        {
+            listItem.push({label:list[i].Name, value:list[i].Id,priority:priority,type:status});
+        }
+      }
+        return listItem;
     }
 
-}
+/*
+---------------File Drag And Drop Methods *START*-----------------------
+*/
 
-public fileDragLeave(fileInput: any){
-//console.log("fileDragLeave");
-var thisObj = this;
-    if(this.dragTimeout != undefined && this.dragTimeout != "undefined"){
-        // console.log("clear---");
-    clearTimeout(this.dragTimeout);
-    }
-     this.dragTimeout = setTimeout(function(){
-     thisObj.hasBaseDropZoneOver = false;
-    },500);
+    /*
+    @params      : fileInput
+    @ParamType   :  any
+    @Description : Enabling the dropzone DIV on dragOver
+    */
     
-}
+    public fileOverBase(fileInput:any):void 
+    {
+        this.hasBaseDropZoneOver = true;
+        if(this.dragTimeout != undefined && this.dragTimeout != "undefined")
+        { 
+            clearTimeout(this.dragTimeout);
+        }
+    }
 
-public fileUploadEvent(fileInput: any, comeFrom: string):void {
-    console.log("the source " + comeFrom);
-   // console.log("cahnge event " + fileInput.name +"------- " + fileInput.size);
-   if(comeFrom == 'fileChange'){
-        this.filesToUpload = <Array<File>> fileInput.target.files;
-   } else if(comeFrom == 'fileDrop'){
-        this.filesToUpload = <Array<File>> fileInput.dataTransfer.files;
-   } else{
-        this.filesToUpload = <Array<File>> fileInput.target.files;
-   }
+     /*
+    @params      : fileInput
+    @ParamType   :  any
+    @Description : Disabling the dropzone DIV on dragOver
+    */
+    public fileDragLeave(fileInput: any){
+    var thisObj = this;
+        if(this.dragTimeout != undefined && this.dragTimeout != "undefined"){
+        clearTimeout(this.dragTimeout);
+        }
+         this.dragTimeout = setTimeout(function(){
+         thisObj.hasBaseDropZoneOver = false;
+        },500);
         
-        this.hasBaseDropZoneOver = false;
-        this.fileUploadStatus = true;
-        this.fileUploadService.makeFileRequest(GlobalVariable.FILE_UPLOAD_URL, [], this.filesToUpload).then((result :Array<any>) => {
+    }
 
-            for(var i = 0; i<result.length; i++){
-                var uploadedFileExtension = (result[i].originalname).split('.').pop();
-                if(uploadedFileExtension == "png" || uploadedFileExtension == "jpg" || uploadedFileExtension == "jpeg" || uploadedFileExtension == "gif") {
-                    this.form['description'] = this.form['description'] + "[[image:" +result[i].path + "|" + result[i].originalname + "]] ";
-                } else{
-                    this.form['description'] = this.form['description'] + "[[file:" +result[i].path + "|" + result[i].originalname + "]] ";
+    /*
+    @params       : fileInput,comeFrom
+    @ParamType    :  any,string
+    @Description  : Uploading File
+    */
+    public fileUploadEvent(fileInput: any, comeFrom: string):void 
+    {
+       if(comeFrom == 'fileChange') {
+            this.filesToUpload = <Array<File>> fileInput.target.files;
+       } else if(comeFrom == 'fileDrop') {
+            this.filesToUpload = <Array<File>> fileInput.dataTransfer.files;
+       } else {
+            this.filesToUpload = <Array<File>> fileInput.target.files;
+       }
+            
+            this.hasBaseDropZoneOver = false;
+            this.fileUploadStatus = true;
+            this.fileUploadService.makeFileRequest(GlobalVariable.FILE_UPLOAD_URL, [], this.filesToUpload).then(
+                (result :Array<any>) => {
+    
+                for(var i = 0; i<result.length; i++){
+                    var uploadedFileExtension = (result[i].originalname).split('.').pop();
+                    if(uploadedFileExtension == "png" || uploadedFileExtension == "jpg" || uploadedFileExtension == "jpeg" || uploadedFileExtension == "gif") {
+                        this.form['description'] = this.form['description'] + "[[image:" +result[i].path + "|" + result[i].originalname + "]] ";
+                    } else{
+                        this.form['description'] = this.form['description'] + "[[file:" +result[i].path + "|" + result[i].originalname + "]] ";
+                    }
                 }
-            }
-            this.fileUploadStatus = false;
-        }, (error) => {
-            console.error(error);
-            this.form['description'] = this.form['description'] + "Error while uploading";
-            this.fileUploadStatus = false;
-        });
-}
+                this.fileUploadStatus = false;
+            }, (error) => {
+                console.error("Error occured in story-formcomponent::fileUploadEvent"+error);
+                this.form['description'] = this.form['description'] + "Error while uploading";
+                this.fileUploadStatus = false;
+            });
+    }
+    
 
-saveStory(){
-    console.log("post____data");
-     this._service.saveStory(this.form,(response)=>{
-     });
-}
+/*
+------------File Upload Methods **END**--------------------
+*/
+
+    /*
+    @Description:Creating Ticket/Story 
+
+    */
+    saveStory(){
+         this._service.saveStory(this.form,(response)=>{
+         });
+    }
 
 }
--- ProjectX/src/app/services/file-upload.service.ts
+++ ProjectX/src/app/services/file-upload.service.ts
@@ -11,16 +11,13 @@ public makeFileRequest(url: string, params: Array<string>, files: Array<File>) {
         return new Promise((resolve, reject) => {
             var formData: any = new FormData();
             var xhr = new XMLHttpRequest();
-            // console.log("files length "+files.length);
             for(var i = 0; i < files.length; i++) { 
                 formData.append("uploads[]", files[i], files[i].name);
             }
-              //formData.append("uploads[]", files, files.name);
 
             xhr.onreadystatechange = function () {
                 if (xhr.readyState == 4) {
                     if (xhr.status == 200) {
-                        //console.log("the responc " + JSON.parse(xhr.response))
                         resolve(JSON.parse(xhr.response));
                     } else {
                         reject(xhr.response);
