f1f5132ec75c580e9261b51498c120e7d6c06014
MadanGopal <MadanGopal@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-02-28 06:30:40 +0000

    Console DB parameters added in main-local.php
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/branches@351 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- ProjectXService/console/config/main-local.php
+++ ProjectXService/console/config/main-local.php
@@ -4,4 +4,18 @@ return [
     'modules' => [
         'gii' => 'yii\gii\Module',
     ],
+    'components' => [
+    'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=10.10.73.33;dbname=Techo2_ProjectX',
+            'username' => 'root',
+            'password' => 'techo2',
+            'charset' => 'utf8',
+        ],
+        'mongodb' => [
+            'class' => 'yii\mongodb\Connection',
+            'dsn' => 'mongodb://10.10.73.33:27017/Techo2_ProjectX',
+
+        ],
+     ]
 ];
