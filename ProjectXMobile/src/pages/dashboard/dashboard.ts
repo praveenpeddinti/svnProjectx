2928899db6339f5c7fd69fa7ba025837c688e3fd
udaykumar.kalluri <udaykumar.kalluri@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-03-07 13:16:30 +0000

    Added infinite scrolling functionality.
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/branches@389 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- ProjectXMobile/src/pages/dashboard/dashboard.ts
+++ ProjectXMobile/src/pages/dashboard/dashboard.ts
@@ -1,12 +1,12 @@
-import {Component} from '@angular/core';
-import {NavController, NavParams, AlertController, ViewController, LoadingController, PopoverController} from 'ionic-angular';
-import {HomePage} from '../home/home';
-import {LoginPage} from '../login/login';
-import {Storage} from "@ionic/storage";
-import {PopoverPage} from '../popover/popover';
-import {StoryDetailsPage} from '../story-details/story-details';
-import {Globalservice} from '../../providers/globalservice';
-import {Constants} from '../../providers/constants';
+import { Component } from '@angular/core';
+import { NavController, NavParams, AlertController, ViewController, LoadingController, PopoverController } from 'ionic-angular';
+import { HomePage } from '../home/home';
+import { LoginPage } from '../login/login';
+import { Storage } from "@ionic/storage";
+import { PopoverPage } from '../popover/popover';
+import { StoryDetailsPage } from '../story-details/story-details';
+import { Globalservice } from '../../providers/globalservice';
+import { Constants } from '../../providers/constants';
 
 /*
   Generated class for the Dashboard page.
@@ -20,12 +20,15 @@ import {Constants} from '../../providers/constants';
 })
 export class DashboardPage {
     public items: Array<any>;
-    public arrayObject: Array<{id: string, title: string, assignTo: string, priority: string, workflow: string, bucket: string, duedate: string}>;
+    public start: number = 10;
+    public offsetIndex: number = 0;
+    public arrayObject: Array<{ id: string, title: string, assignTo: string, priority: string, workflow: string, bucket: string, duedate: string }>;
+    public moreDataLoaded: boolean = true;
 
     userName: any = '';
     userPassword: string = '';
     userToken: any = '';
-    paramas = {"projectId": 1, "offset": 0, "pagesize": 10, "sortvalue": "Id", "sortorder": "desc", "userInfo": {"Id": "9", "username": "hareesh.bekkam", "token": "a1a3d7b95e950cbc1cb7@9"}};
+    paramas = {"projectId": 1, "offset": this.offsetIndex, "pagesize": this.start, "sortvalue": "Id", "sortorder": "asc", "userInfo": { "Id": "9", "username": "hareesh.bekkam", "token": "045cdabd2bfc0bc46571@9" } };
     constructor(public navCtrl: NavController,
         public navParams: NavParams,
         public loadingController: LoadingController,
@@ -40,46 +43,60 @@ export class DashboardPage {
             console.log("in did load " + value.username);
             this.userName = value.username;
         });
+        this.arrayObject = [];
 
     }
 
     ionViewDidLoad() {
         console.log('ionViewDidLoad DashboardPage');
-         this.getAllStoriesList();
+        this.getAllStoriesList();
     }
     openPopover(myEvent) {
-        let userCredentials = {username: this.userName};
+        let userCredentials = {username: this.userName };
         let popover = this.popoverCtrl.create(PopoverPage, userCredentials);
         console.log("User name is " + this.userName);
         popover.present({
             ev: myEvent
         });
     }
-    
+
     /**
         used for getting all the stories 
         author uday   
     
      */
     getAllStoriesList(): void {
+       
+        if (this.paramas.offset == 0) {
+            this.paramas.offset = 0;
+        }
+        console.log("params are ");
+        console.log(this.paramas);
         this.globalService.getStoriesList(this.urlConstants.getAllTicketDetails, this.paramas).subscribe(
             data => {
                 this.items = data.data;
                 console.log("the count value is " + this.items.length);
-                this.arrayObject = [];
-                for (let i = 0; i < this.items.length; i++) {
-                    var _id = this.items[i][0].field_value;
-                    var _title = this.items[i][1].field_value;
-                    var _assignTo = this.items[i][2].field_value;
-                    var _priority = this.items[i][3].field_value;
-                    var _workflow = this.items[i][4].field_value;
-                    var _bucket = this.items[i][5].field_value;
-                    var _dudate = this.items[i][6].field_value;
+
+                if (this.items.length == 0) {
+                    this.moreDataLoaded = false;
+                }
+                //console.log(this.paramas);
+                console.log(this.items);
+
+                for (let ticket = 0; ticket < this.items.length; ticket++) {
+                    var _id = this.items[ticket][0].field_value;
+                    var _title = this.items[ticket][1].field_value;
+                    var _assignTo = this.items[ticket][2].field_value;
+                    var _priority = this.items[ticket][3].field_value;
+                    var _workflow = this.items[ticket][4].field_value;
+                    var _bucket = this.items[ticket][5].field_value;
+                    var _dudate = this.items[ticket][6].field_value;
 
                     this.arrayObject.push({
                         id: _id, title: _title, assignTo: _assignTo, priority: _priority, workflow: _workflow, bucket: _bucket, duedate: _dudate
                     });
                 }
+                this.paramas.offset = (this.paramas.offset) + 1;
             },
             error => {
                 console.log("the error " + JSON.stringify(error));
@@ -87,7 +104,7 @@ export class DashboardPage {
             () => console.log('login api call complete')
         );
     }
-    
+
     /**
       used for click event on list
       author uday   
@@ -105,4 +122,20 @@ export class DashboardPage {
         this.navCtrl.push(StoryDetailsPage);
     }
 
+    doInfinite(infiniteScroll) {
+
+        console.log("sample scrolling");
+
+        setTimeout(() => {
+            if (this.moreDataLoaded == true) {
+                console.log('Async operation has ended');
+                this.getAllStoriesList();
+                infiniteScroll.complete();
+            } else {
+                console.log('failing condition');
+                infiniteScroll.complete();
+            }
+        }, 2000);
+
+    }
 }
