834e03b8a71c241cb573f601aa5adee0494fb9bc
udaykumar.kalluri <udaykumar.kalluri@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-03-09 11:09:44 +0000

    Added comments and Passed User info to the web service from login screen stored value.
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/branches@419 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- ProjectXMobile/src/pages/dashboard/dashboard.ts
+++ ProjectXMobile/src/pages/dashboard/dashboard.ts
@@ -18,15 +18,28 @@ import { Constants } from '../../providers/constants';
 })
 export class DashboardPage {
     public items: Array<any>;
-    public start: number = 10;
-    public offsetIndex: number = 0;
+    public start: number = 10;//no of items showing in each page by default 10
+    public offsetIndex: number = 0;//offset Index default value is 0 while pulling the list screen down the value will be incremented
+
+    /*
+    ***
+    arrayObject is used for saving the stories list and passing it to the html page
+     ** 
+    */
     public arrayObject: Array<{ id: string, title: string, assignTo: string, priority: string, workflow: string, bucket: string, duedate: string }>;
     public moreDataLoaded: boolean = true;
 
     userName: any = '';
-    userPassword: string = '';
-    userToken: any = '';
-    paramas = { "projectId": 1, "offset": this.offsetIndex, "pagesize": this.start, "sortvalue": "Id", "sortorder": "asc", "userInfo": { "Id": "9", "username": "hareesh.bekkam", "token": "045cdabd2bfc0bc46571@9" } };
+    // userPassword: string = '';
+    // userToken: any = '';
+
+    /*
+    *
+        paramas are used while getting List Results from the webservice.
+    *
+    */
+
+    paramas = { "projectId": 1, "offset": this.offsetIndex, "pagesize": this.start, "sortvalue": "Id", "sortorder": "asc", "userInfo": { "Id": "", "username": "", "token": "" } };
     constructor(public navCtrl: NavController,
         public navParams: NavParams,
         public loadingController: LoadingController,
@@ -36,11 +49,6 @@ export class DashboardPage {
         public viewCtrl: ViewController,
         private globalService: Globalservice,
         private urlConstants: Constants) {
-
-        this.storage.get('userCredentials').then((value) => {
-            this.userName = value.username;
-            this.userToken = value.userToken;
-        });
         this.arrayObject = [];
 
     }
@@ -51,15 +59,29 @@ export class DashboardPage {
        */
 
     ionViewDidLoad() {
-       
-        this.paramas.userInfo.username = this.userName;
-        this.paramas.userInfo.token = this.userToken;
+
+        /** 
+         * User Local storage details getting logic.
+         
+         * **/
+
+        this.storage.get('userCredentials').then((value) => {
+            this.userName = value.username;
+            this.paramas.userInfo.Id = value.Id;
+            this.paramas.userInfo.username = value.username;
+            this.paramas.userInfo.token = value.token;
+        });
+
+        /** 
+         * Displaying stories list when first time loading the screen.
+        * 
+        * **/
         this.getAllStoriesList();
     }
 
 
     /**
-           pop for logout
+           pop up for logout
        
         */
     openPopover(myEvent) {
@@ -81,17 +103,17 @@ export class DashboardPage {
         if (this.paramas.offset == 0) {
             this.paramas.offset = 0;
         }
-        
+
         this.globalService.getStoriesList(this.urlConstants.getAllTicketDetails, this.paramas).subscribe(
             data => {
                 this.items = data.data;
                 console.log("the count value is " + this.items.length);
 
+                console.log("params are" + JSON.stringify(this.paramas));
+
                 if (this.items.length == 0) {
                     this.moreDataLoaded = false;
                 }
-               
-
                 for (let ticket = 0; ticket < this.items.length; ticket++) {
                     var _id = this.items[ticket][0].field_value;
                     var _title = this.items[ticket][1].field_value;
@@ -108,7 +130,7 @@ export class DashboardPage {
                 this.paramas.offset = (this.paramas.offset) + 1;
             },
             error => {
-               
+
             },
             () => console.log('login api call complete')
         );
