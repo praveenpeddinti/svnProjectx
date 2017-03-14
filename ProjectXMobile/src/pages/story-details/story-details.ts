2df0a1557204d66754f855f885beeefba41df337
hareesh.bekkam <hareesh.bekkam@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-03-14 12:30:34 +0000

    remove unwanted's
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/branches@493 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- ProjectXMobile/src/pages/story-details/story-details.ts
+++ ProjectXMobile/src/pages/story-details/story-details.ts
@@ -1,4 +1,4 @@
-import {Component,ViewChild} from '@angular/core';
+import {Component} from '@angular/core';
 import {NavController, NavParams, MenuController, LoadingController} from 'ionic-angular';
 
 import {Globalservice} from '../../providers/globalservice';
