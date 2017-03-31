b37460ecb7301edc059f88886a8a2f9852c8e8e7
allamprabhu.aloji <allamprabhu.aloji@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-03-31 05:52:02 +0000

    story create open gallery.
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/branches@716 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- ProjectXMobile/src/pages/story-create/story-create.ts
+++ ProjectXMobile/src/pages/story-create/story-create.ts
@@ -1,13 +1,16 @@
 import {Component} from '@angular/core';
-import {NavController, NavParams} from 'ionic-angular';
+import {NavController, NavParams,Platform } from 'ionic-angular';
 import {Globalservice} from '../../providers/globalservice';
 import {Constants} from '../../providers/constants';
+import { Camera } from 'ionic-native';
 
 @Component({
     selector: 'page-story-create',
     templateUrl: 'story-create.html'
 })
 export class StoryCreatePage {
+    base64Image
+    file: File;
     public showEditableFieldOnly = [];
     private submitted: boolean = false;
     constructor(public navCtrl: NavController,
@@ -17,10 +20,10 @@ export class StoryCreatePage {
 
         this.globalService.newStoryTemplate();
     }
-
-    onStoryCreate(form): void {
-        console.log("in onStroyCreate");
-        var params = {
+     
+    onStoryCreate(form): void { 
+      console.log("create task submit button"); 
+       var params = {
             "title": "Title Mobile",
             "description": "<p>Testing Creation</p>\n",
             "tasks": ["UI",
@@ -30,5 +33,30 @@ export class StoryCreatePage {
             "priority": "2"
         }
         this.globalService.createStoryORTask(this.constants.createStory, params);
+ }
+ 
+// onChange(event) {
+//    let file = event.srcElement.files;
+//    let postData = {field1:"field1", field2:"field2"}; // Put your form data variable. This is only example.
+////    Constants.postWithFile(this.baseUrl + "add-update",postData,file).then(result => {
+////        console.log(result);
+////    });
+//}
+ onChange(event: EventTarget) {
+        let eventObj: MSInputMethodContext = <MSInputMethodContext> event;
+        let target: HTMLInputElement = <HTMLInputElement> eventObj.target;
+        let files: FileList = target.files;
+        this.file = files[0];
+        console.log(this.file);
     }
-}
+    accessGallery(){
+   Camera.getPicture({
+     sourceType: Camera.PictureSourceType.SAVEDPHOTOALBUM,
+     destinationType: Camera.DestinationType.DATA_URL
+    }).then((imageData) => {
+      this.base64Image = 'data:image/jpeg;base64,'+imageData;
+     }, (err) => {
+      console.log(err);
+    });
+  }
+  }
