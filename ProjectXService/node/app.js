1e8818c140930bfd394e341c879ecfbfd3e7012a
marshal.ryan <marshal.ryan@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-04-13 15:34:23 +0000

    node file commit
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/branches@834 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- ProjectXService/node/app.js
+++ ProjectXService/node/app.js
@@ -12,8 +12,9 @@ var exec = require('child_process').exec;
 var child;
 console.log("entry");
  
-app.use(bodyParser.json());
+
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
  
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -33,9 +34,11 @@ console.log("the file-------- " );
     }
     
 });
-app.post("/assignedTo", type, function(req, res, cb) {
-console.log("assignedTo-------- " );
-  child=spawn(dir+"/yii",['notifications/hello','100']);
+app.post("/assignedTo",function(req, res, cb) {
+//console.log(req.body+"----assignedTo-------- "+req+"-----"+req.body);
+var request = req.body;
+console.log("===collaborator=="+request.ticketId);
+  child=spawn(dir+"/yii",['notifications/assigned-notify',JSON.stringify(request)]);
         child.stdout.setEncoding('utf-8');
         child.stdout.on('data', function(data) {
           console.log("assignedTo-------result- " );
@@ -46,34 +49,73 @@ console.log("assignedTo-------- " );
             console.log("assignedTo-------error- "+data );
        });
     
-//    if (!req.files) {
-//        res.send('No files were uploaded.');
-//    } else{
-//        //console.log("the responce -------- " + req.files + " --- " + JSON.stringify(req.files));
-//        //console.log( "the new file name " + typeof JSON.stringify(req.files['filename']) + "|" + typeof req.files.originalname );
-//      //  res.send(req.files);
-//    }
+});
+
+app.post("/follow",function(req, res, cb) {
+//console.log(req.body+"----assignedTo-------- "+req+"-----"+req.body);
+var request = req.body;
+console.log("===collaborator=="+request.ticketId);
+  child=spawn(dir+"/yii",['notifications/follow-notify',JSON.stringify(request)]);
+        child.stdout.setEncoding('utf-8');
+        child.stdout.on('data', function(data) {
+          console.log("assignedTo-------result- " );
+             res.send(data);
+       });
+          child.stderr.on('data', function(data) {
+//            logger.trace('stderr: ' + data);
+            console.log("assignedTo-------error- "+data );
+       });
     
+});
+
+app.post("/getAllNotifications",function(req,res,cb)
+{
+    var request = req.body;
+    console.log(JSON.stringify(request));
+  child=spawn(dir+"/yii",['notifications/get-all-notifications',JSON.stringify(request)]);
+        child.stdout.setEncoding('utf-8');
+        child.stdout.on('data', function(data) {
+          console.log("assignedTo-------result- " +data);
+             res.send(data);
+       });
+          child.stderr.on('data', function(data) {
+//            logger.trace('stderr: ' + data);
+            console.log("assignedTo-------error- "+data );
+       });
+});
+
+app.post("/propertyChange",function(req,res,cb)
+{
+    var request = req.body;
+  child=spawn(dir+"/yii",['notifications/change-property',JSON.stringify(request)]);
+        child.stdout.setEncoding('utf-8');
+        child.stdout.on('data', function(data) {
+          console.log("assignedTo-------result- " +data);
+             res.send(data);
+       });
+          child.stderr.on('data', function(data) {
+//            logger.trace('stderr: ' + data);
+            console.log("assignedTo-------error- "+data );
+       });
 });
 
 
 
- var http = require('http').createServer(app);
- http.listen(4201);
-//var server = app.listen(4201, function() {
-//    console.log("Listening on port %s...", server.address().port);
-//});
-var io = require('socket.io')(http);
-io.sockets.on('connection', function(client)
-{  
-    
-    client.on('assignedTo', function(collaborator) {
-        console.log('assignedTo-----'+collaborator);
-        child=spawn(dir+"/yii",['notifications/hello','100']);
-        child.stdout.setEncoding('utf-8');
-        child.stdout.on('data', function(res) {
-            console.log(res);
-       });
-    });
 
-})
+
+ //var http = require('http').createServer(app);
+ app.listen(4201);
+// var io = require('socket.io')(http);
+//    io.sockets.on('connection', function(client)
+//    {  
+//
+//        client.on('assignedTo', function(collaborator) {
+//            console.log('assignedTo-----'+collaborator);
+//            child=spawn(dir+"/yii",['notifications/hello','100']);
+//            child.stdout.setEncoding('utf-8');
+//            child.stdout.on('data', function(res) {
+//                console.log(res);
+//           });
+//       });
+//
+//    });
