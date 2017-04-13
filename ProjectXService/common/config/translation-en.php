32239defafaed3274cd5e8018999fd6a1cbefcaa
marshal.ryan <marshal.ryan@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-04-13 13:29:52 +0000

    Translation file added
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/branches@827 b02b8b71-e8a6-4a5c-b3be-45d727391186

---
+++ ProjectXService/common/config/translation-en.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    "mention" => "has mentioned",
+    "changed" =>"has changed",
+    "assignedTo" => "assigned",
+    "stakeholder"=>"as a stakeholder",
+    "comment"=>"commented on",
+    "reply"=>"replied on",
+    "addedyou"=>"added you as follower",
+    "added"=>"added",
+    "removed"=>"removed",
+    "removedyou"=>"removed you as follower",
+    "follower"=>"as follower",
+    "set"=>"set to"
+    
+];
+
