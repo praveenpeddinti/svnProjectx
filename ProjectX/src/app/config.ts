a9a521ac1995897106a6242d88ebcb99e30614bc
marshal.ryan <marshal.ryan@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-04-13 15:25:22 +0000

    Notification url added
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/branches@833 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- ProjectX/src/app/config.ts
+++ ProjectX/src/app/config.ts
@@ -3,8 +3,9 @@ export const GlobalVariable = Object.freeze({
      //BASE_API_URL :'http://10.10.73.33/',
     // FILE_UPLOAD_URL: 'http://10.10.73.33:4201/upload'
 
-     BASE_API_URL :'http://10.10.73.77/',
-     FILE_UPLOAD_URL: 'http://10.10.73.77:4201/upload'
+     BASE_API_URL :'http://10.10.73.39:8050/',
+     FILE_UPLOAD_URL: 'http://10.10.73.39:4201/upload',
+     NOTIFICATION_URL:'http://10.10.73.39:4201'
 
-     //... more of your variables
+     //... more of your  variables
  });
