b5358eb582d68f8de8bf121b0d226fe87b1a90a4
moinhussain <moinhussain@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-04-15 10:34:17 +0000

    CommonUtilityTwo.php is commited
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/branches@870 b02b8b71-e8a6-4a5c-b3be-45d727391186

---
+++ ProjectXService/common/components/CommonUtilityTwo.php
@@ -0,0 +1,19 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace common\components;
+use common\models\mongo\{TicketCollection,TinyUserCollection,TicketArtifacts};
+use common\models\mysql\{Priority,Projects,WorkFlowFields,Bucket,TicketType,StoryFields,StoryCustomFields,PlanLevel,MapListCustomStoryFields};
+use Yii;
+
+ /*
+ * @author Moin Hussain
+ */
+
+class CommonUtilityTwo {
+    
+}
