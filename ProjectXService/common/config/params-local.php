2d80e99f08da4faae3895eff07f8a03492bae824
marshal.ryan <marshal.ryan@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-04-19 14:18:16 +0000

    Added AppURL for the email link.
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/branches@962 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- ProjectXService/common/config/params-local.php
+++ ProjectXService/common/config/params-local.php
@@ -1,7 +1,8 @@
 <?php
 return [
-    "ServerURL" => "http://10.10.73.33",
+    "ServerURL" => "http://10.10.73.39:8050",
     "ProjectRoot" =>"/usr/share/nginx/www/ProjectXService/frontend/web",
     "StoryArtifactPath" => "/files/story",
-    "ProjectEmail" => "moin.hussain@techo2.com"
+    "ProjectEmail" => "moin.hussain@techo2.com",
+    "AppURL"=> "http://10.10.73.39:4200"
 ];
