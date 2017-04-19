9ff7522a20fdde09d58b0b3403758d3823facfac
moinhussain <moinhussain@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-04-19 07:12:33 +0000

    fixed issues
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/branches@936 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- ProjectXService/common/config/params-local.php
+++ ProjectXService/common/config/params-local.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    "ServerURL" => "http://10.10.73.77",
+    "ServerURL" => "http://10.10.73.33",
     "ProjectRoot" =>"/usr/share/nginx/www/ProjectXService/frontend/web",
     "StoryArtifactPath" => "/files/story",
-    
+    "ProjectEmail" => "moin.hussain@techo2.com"
 ];
