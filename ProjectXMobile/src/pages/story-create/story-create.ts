fcb82cb1b9633c60a270be77d9650df9155260f3
allamprabhu.aloji <allamprabhu.aloji@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-04-25 08:50:45 +0000

    Removed unwanted code.
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/branches@1034 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- ProjectXMobile/src/pages/story-create/story-create.ts
+++ ProjectXMobile/src/pages/story-create/story-create.ts
@@ -27,7 +27,7 @@ export class StoryCreatePage {
     public previousSelectIndex: any;
     public previousSelectedValue = "";
     public readOnlyDropDownField: boolean = false;
-    private submitted: boolean = false;
+    // private submitted: boolean = false;
     public displayedClassColorValue="";
     lastImage: string = null;
     
@@ -47,55 +47,55 @@ export class StoryCreatePage {
         this.storage.get('userCredentials').then((value) => {
             this.userName = value.username;
         });
-        
+
         globalService.newStoryTemplate(this.constants.templateForStoryCreation, this.navParams.get("id")).subscribe(
-    (result) => {
-        this.itemfield = result.data.story_fields;
-        console.log("result newStoryTemplate " + JSON.stringify(this.itemfield));
-        this.tasktypes = result.data.task_types;
-        
-        console.log("result newStoryTemplate " + JSON.stringify(this.tasktypes));
-        this.templatedataList = [];
-        this.tasktypeList = [];
+            (result) => {
+                this.itemfield = result.data.story_fields;
+                console.log("result newStoryTemplate " + JSON.stringify(this.itemfield));
+                this.tasktypes = result.data.task_types;
 
-            for (let i = 0; i < this.itemfield.length; i++) {
-                var _id = this.itemfield[i].Id;
-                var _title = this.itemfield[i].Title;
-                var _defaultValue = this.itemfield[i].DefaultValue;
-                var _assignData = this.itemfield[i].data;
-                var _readOnly = this.itemfield[i].readonly;
-                var _fieldType = this.itemfield[i].field_type;
-                var _fieldName = this.itemfield[i].Field_Name;
-                if(_fieldName == 'planlevel'){
-                    this.create.planlevel = _defaultValue;
-                } else if(_fieldName == 'priority') {
-                    this.create.priority = _defaultValue;
-                    this.displayedClassColorValue = _assignData[2].Name;
+                console.log("result newStoryTemplate " + JSON.stringify(this.tasktypes));
+                this.templatedataList = [];
+                this.tasktypeList = [];
+
+                for (let i = 0; i < this.itemfield.length; i++) {
+                    var _id = this.itemfield[i].Id;
+                    var _title = this.itemfield[i].Title;
+                    var _defaultValue = this.itemfield[i].DefaultValue;
+                    var _assignData = this.itemfield[i].data;
+                    var _readOnly = this.itemfield[i].readonly;
+                    var _fieldType = this.itemfield[i].field_type;
+                    var _fieldName = this.itemfield[i].Field_Name;
+                    if (_fieldName == 'planlevel') {
+                        this.create.planlevel = _defaultValue;
+                    } else if (_fieldName == 'priority') {
+                        this.create.priority = _defaultValue;
+                        this.displayedClassColorValue = _assignData[2].Name;
+                    }
+                    this.templatedataList.push({
+                        id: _id, title: _title, defaultValue: _defaultValue, assignData: _assignData, readOnly: _readOnly, fieldType: _fieldType, fieldName: _fieldName
+                    });
                 }
-                this.templatedataList.push({
-                    id: _id, title: _title, defaultValue: _defaultValue, assignData: _assignData, readOnly: _readOnly, fieldType: _fieldType, fieldName: _fieldName
-                });
+
+                for (let j = 0; j < this.tasktypes.length; j++) {
+                    var _id = this.tasktypes[j].Id;
+                    var _Name = this.tasktypes[j].Name;
+                    var _IsDefault = this.tasktypes[j].IsDefault;
+                    var _selected = false;
+                    if (_IsDefault == "1") {
+                        _selected = true;
+                    }
+
+                    this.tasktypeList.push({
+                        Id: _id, Name: _Name, IsDefault: _IsDefault, selected: _selected
+                    });
+
+                }
+
+            }, (error) => {
+                console.log("the error in ticker derais " + JSON.stringify(error));
             }
-
-            for (let j = 0; j < this.tasktypes.length; j++) {
-                var _id = this.tasktypes[j].Id;
-                var _Name = this.tasktypes[j].Name;
-                var _IsDefault = this.tasktypes[j].IsDefault;
-                var _selected = false;
-                if (_IsDefault == "1") {
-                    _selected = true;
-                }
-
-                this.tasktypeList.push({
-                    Id: _id, Name: _Name, IsDefault: _IsDefault, selected: _selected
-                });
-
-            }
-                
-                }, (error) => {
-                    console.log("the error in ticker derais " + JSON.stringify(error));
-                }
-            );
+        );
     }
 
     ionViewDidLoad() {
@@ -159,7 +159,8 @@ export class StoryCreatePage {
             //document.getElementById("item_"+index).classList.remove("item-select");
         }, 300);
     }
-     public presentActionSheet() {
+    //Open action sheet for File upload
+    public presentActionSheet() {
         let actionSheet = this.actionSheetCtrl.create({
             title: 'Select Image Source',
             buttons: [
@@ -216,136 +217,113 @@ export class StoryCreatePage {
         }, (err) => {
             this.presentToast('Error while selecting image.');
         });
-    }
-
- private createFileName(originalName) {
-        // console.log('createFileName');
-        var d = new Date(),
-        n = d.getTime(),
-        // newFileName =  n + ".jpg";
-        newFileName =  "image"+n;
-        return newFileName;
-    }
-       private copyFileToLocalDir(namePath, currentName, newFileName) {
-        // console.log('copyFileToLocalDir');
-        File.copyFile(namePath, currentName, cordova.file.dataDirectory, newFileName).then(success => {
-            this.lastImage = newFileName;
-            this.uploadImage(currentName, newFileName);
-        }, error => {
-            this.presentToast('Error while storing file.');
-        });
-    }
-
-       public uploadImage(originalname, savedname) {
-        // console.log('uploadImage');
-        // Destination URL
-        var url = this.constants.filesUploading;
-        // File for Upload
-        var targetPath = this.pathForImage(this.lastImage);
-        // File name only
-        var filename = this.lastImage;
-        var options = {
-            fileKey: "commentFile",
-            fileName: filename,
-            chunkedMode: false,
-            mimeType: "image/jpeg",
-            params : {'filename': filename,'directory':this.constants.fileUploadsFolder,'originalname': originalname}
-        };
-        const fileTransfer = new Transfer();
-        // Use the FileTransfer to upload the image
-        fileTransfer.upload(targetPath, url, options).then((data) => {
-            // console.log('data'+JSON.stringify(data));
-            this.uploadedInserver(data);
-        }, (err) => {
-            console.log('Error while uploading file.'+ JSON.stringify(err));
-        });
-    }
-    public pathForImage(img) {
-        if (img === null) {
-            return '';
-        } else {
-            return cordova.file.dataDirectory + img;
         }
-    }
-        public uploadedInserver(dataUploaded){
-        // console.log('uploadedInserver');
-        var serverResponse = JSON.parse(dataUploaded.response);
-        if (serverResponse['status'] == '1') {
-            var uploadedFileExtension = (serverResponse['originalname']).split('.').pop();
-            if (uploadedFileExtension == "png" || uploadedFileExtension == "jpg" || uploadedFileExtension == "jpeg" || uploadedFileExtension == "gif") {
-                this.create.description = this.create.description + "[[image:" +serverResponse['path'] + "|" + serverResponse['originalname'] + "]] ";
-            }
-            console.log('Image succesfully uploaded.');
-            // this.presentToast('Image succesfully uploaded.');
-        }else{
-            console.log('Error while uploading file.');
-            // this.presentToast('Error while uploading file.');
+
+        private createFileName(originalName) {
+            // console.log('createFileName');
+            var d = new Date(),
+                n = d.getTime(),
+                // newFileName =  n + ".jpg";
+                newFileName = "image" + n;
+            return newFileName;
         }
-    }
+        private copyFileToLocalDir(namePath, currentName, newFileName) {
+            // console.log('copyFileToLocalDir');
+            File.copyFile(namePath, currentName, cordova.file.dataDirectory, newFileName).then(success => {
+                this.lastImage = newFileName;
+                this.uploadImage(currentName, newFileName);
+            }, error => {
+                this.presentToast('Error while storing file.');
+            });
+        }
 
-    //     public takePicture(sourceType) {
-    //     // Create options for the Camera Dialog
-    //     var options = {
-    //         quality: 100,
-    //         sourceType: sourceType,
-    //         destinationType: Camera.DestinationType.FILE_URI,
-    //         saveToPhotoAlbum: false,
-    //         correctOrientation: true
-    //     };
+        public uploadImage(originalname, savedname) {
+            // console.log('uploadImage');
+            // Destination URL
+            var url = this.constants.filesUploading;
+            // File for Upload
+            var targetPath = this.pathForImage(this.lastImage);
+            // File name only
+            var filename = this.lastImage;
+            var options = {
+                fileKey: "commentFile",
+                fileName: filename,
+                chunkedMode: false,
+                mimeType: "image/jpeg",
+                params: {'filename': filename, 'directory': this.constants.fileUploadsFolder, 'originalname': originalname}
+            };
+            const fileTransfer = new Transfer();
+            // Use the FileTransfer to upload the image
+            fileTransfer.upload(targetPath, url, options).then((data) => {
+                // console.log('data'+JSON.stringify(data));
+                this.uploadedInserver(data);
+            }, (err) => {
+                console.log('Error while uploading file.' + JSON.stringify(err));
+            });
+        }
+       public pathForImage(img) {
+           if (img === null) {
+               return '';
+           } else {
+               return cordova.file.dataDirectory + img;
+           }
+       }
+       public uploadedInserver(dataUploaded) {
+           // console.log('uploadedInserver');
+           var serverResponse = JSON.parse(dataUploaded.response);
+           if (serverResponse['status'] == '1') {
+               var uploadedFileExtension = (serverResponse['originalname']).split('.').pop();
+               if (uploadedFileExtension == "png" || uploadedFileExtension == "jpg" || uploadedFileExtension == "jpeg" || uploadedFileExtension == "gif") {
+                   this.create.description = this.create.description + "[[image:" + serverResponse['path'] + "|" + serverResponse['originalname'] + "]] ";
+               }
+               console.log('Image succesfully uploaded.');
+               // this.presentToast('Image succesfully uploaded.');
+           } else {
+               console.log('Error while uploading file.');
+               // this.presentToast('Error while uploading file.');
+           }
+       }
+    
+        private presentToast(text) {
+            let toast = this.toastCtrl.create({
+                message: text,
+                duration: 3000,
+                position: 'top'
+            });
+            toast.present();
+        }
 
-    //     // Get the data of an image
-    //     Camera.getPicture(options).then((imagePath) => {
-           
-    //         //this.ImageLoc = imagePath;
-    //         this.create.description = "<p>" + this.create.description + imagePath +"</p>";
-    //         //this.base64Image = 'data:image/jpeg;base64,'+imagePath;
-    //     }, (err) => {
-    //         this.presentToast('Error while selecting image.');
-    //     });
-    // }
+        onChange(event: EventTarget) {
+            let eventObj: MSInputMethodContext = <MSInputMethodContext> event;
+            let target: HTMLInputElement = <HTMLInputElement> eventObj.target;
+            let files: FileList = target.files;
+            this.file = files[0];
+            console.log(this.file);
+        }
+        openPopover(myEvent) {
+            let userCredentials = {username: this.userName};
+            let popover = this.popoverCtrl.create(PopoverPage, userCredentials);
+            popover.present({
+                ev: myEvent
+            });
+        }
+        openOptionsModal(fieldDetails, index) {
+            console.log("the model present");
 
-    private presentToast(text) {
-        let toast = this.toastCtrl.create({
-            message: text,
-            duration: 3000,
-            position: 'top'
-        });
-        toast.present();
-    }
-
- onChange(event: EventTarget) {
-        let eventObj: MSInputMethodContext = <MSInputMethodContext> event;
-        let target: HTMLInputElement = <HTMLInputElement> eventObj.target;
-        let files: FileList = target.files;
-        this.file = files[0];
-        console.log(this.file);
-    }
-    openPopover(myEvent) {
-        let userCredentials = { username: this.userName };
-        let popover = this.popoverCtrl.create(PopoverPage, userCredentials);
-        popover.present({
-            ev: myEvent
-        });
-    }
-    openOptionsModal(fieldDetails, index) {
-        console.log("the model present");
-
-        let optionsModal = this.modalController.create(CustomModalPage, { activeField: fieldDetails, activatedFieldIndex: index, displayList: fieldDetails.assignData });
-        optionsModal.onDidDismiss((data) => {
-            console.log("the dismiss data " + index + " ----- " + JSON.stringify(data));
-            if(data != null && Object.keys(data).length > 0){
-                if (fieldDetails.fieldName == "planlevel") {
-                    this.create.planlevel = data.Id;
-                } else if (fieldDetails.fieldName == "priority") {
-                    this.create.priority = data.Id;
-                    this.displayedClassColorValue = data.Name;
+            let optionsModal = this.modalController.create(CustomModalPage, {activeField: fieldDetails, activatedFieldIndex: index, displayList: fieldDetails.assignData});
+            optionsModal.onDidDismiss((data) => {
+                console.log("the dismiss data " + index + " ----- " + JSON.stringify(data));
+                if (data != null && Object.keys(data).length > 0) {
+                    if (fieldDetails.fieldName == "planlevel") {
+                        this.create.planlevel = data.Id;
+                    } else if (fieldDetails.fieldName == "priority") {
+                        this.create.priority = data.Id;
+                        this.displayedClassColorValue = data.Name;
+                    }
+                    jQuery("#field_title_" + index + " div").text(data.Name);
                 }
-
-                // document.getElementById("field_title_" + index).innerHTML = data.Name;
-                jQuery("#field_title_"+index+ " div").text(data.Name);
-             } 
-        });
-        optionsModal.present();
-    }
+            });
+            optionsModal.present();
+        }
 
   }
