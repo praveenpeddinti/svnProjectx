e510d8faa1cbecc71aca8d36f352a9dcbc2f0370
padmajad <padmajad@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-05-03 13:52:10 +0000

    updated code of enhancements of globalsearch
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/branches@1154 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- ProjectX/src/app/components/search/search.component.ts
+++ ProjectX/src/app/components/search/search.component.ts
@@ -16,42 +16,41 @@ export class SearchComponent implements OnInit{
     public stringPosition;
     private page=1;
     public ready=true;
-    
+    public searchFlag;
     ngOnInit(){
      this.route.queryParams.subscribe(
       params => 
       {
              this.searchString=params['v'];
               this.page=1;
-             this.searchArray=[];
-             this.load_contents(this.page,this.searchString);
+           this.searchArray=[];
+             this.load_contents(this.page,this.searchString,this.searchFlag);
            })
-        
-        
        // this.load_contents(this.page);
-
-              var thisObj=this; 
+             var thisObj=this; 
              jQuery(document).ready(function(){
            jQuery(window).scroll(function() {
                 if (thisObj.ready && jQuery(window).scrollTop() >= (jQuery(document).height() - jQuery(window).height())) {
                     thisObj.ready=false;
                     thisObj.page++;
-                    thisObj.load_contents(thisObj.page,thisObj.searchString);  
+                    thisObj.load_contents(thisObj.page,thisObj.searchString,thisObj.searchFlag); 
+                    
                 }
+              
                 });
         })
     }
-   public  load_contents(page,searchString){
+   public  load_contents(page,searchString,searchFlag){
         var post_data={
         'projectId':1,
         'searchString':searchString,
-        'page':page
+        'page':page,
+        'searchFlag':searchFlag
       }
-         this._ajaxService.AjaxSubscribe("site/global-search",post_data,(result)=>
+           this._ajaxService.AjaxSubscribe("site/global-search",post_data,(result)=>
          { 
-                 if(result.status !='401'){ 
-                    jQuery('#searchsection').html('');
-                    this.searchArray= this.searchDataBuilder(result.data,this.searchArray);
+                  if(result.status !='401'){ 
+                     this.searchArray= this.searchDataBuilder(result.data,this.searchArray);
                     this.ready=true;
                     }else{
                  jQuery('#searchsection').html('No Results Found');
@@ -77,6 +76,13 @@ export class SearchComponent implements OnInit{
     }
     navigateToStoryDetail(ticketId,slug){
          this._router.navigate(['/story-detail',ticketId],{queryParams: {Slug:slug}});
+     }
+     callsearchByClick(searchFlag){
+        this.searchArray=[];
+        this.searchFlag=searchFlag;
+        this.page=1;
+        this.load_contents(this.page,this.searchString,this.searchFlag);
+        
      }
 }
 
