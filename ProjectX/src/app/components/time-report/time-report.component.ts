2cc5c2eabc435f17274a83be7c49112488476445
padmajad <padmajad@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-05-18 12:56:27 +0000

    time report
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/branches@1277 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- ProjectX/src/app/components/time-report/time-report.component.ts
+++ ProjectX/src/app/components/time-report/time-report.component.ts
@@ -5,6 +5,8 @@ import { AjaxService } from '../../ajax/ajax.service';
 import { Router } from '@angular/router';
 import { GlobalVariable } from '../../config';
 import { Http, Headers } from '@angular/http';
+import {AccordionModule,DropdownModule,SelectItem} from 'primeng/primeng';
+//import {AutoCompleteModule} from 'primeng/primeng';
 declare var jQuery:any;
 
 @Component({
@@ -17,13 +19,22 @@ declare var jQuery:any;
 
 export class TimeReportComponent{
     public FilterList=[];
-     public selectedFilter=null;                  
+    public selectedFilter=null; 
+    private search_results:string[];
+    private ticketdesc=[];
+    private text:string;
+    private dateVal = new Date();
+    private selectedValForTask="";
+    private selectedValForDate:Date;
+    private calendarVal = new Date();
     @ViewChild('myTable') table: any;
     rows = [];
     row1 = [];
     count: number = 0;
     offset: number = 0;
-    limit: number = 10;
+
+    limit: number = 5;
+
     sortvalue: string = "Id";
     sortorder: string = "desc";
     loading: boolean = false;
@@ -58,14 +69,14 @@ export class TimeReportComponent{
                     sortby: '',
                     class: 'arrowClass'
                 }
-                
+
               ];
 
 expanded: any = {};
     headers = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' });
     constructor(
         private _router: Router,
-        private _service: TimeReportService, private http: Http) { console.log("in constructor"); }
+        private _service: TimeReportService, private http: Http,private _ajaxService: AjaxService) { console.log("in constructor"); }
 
     ngOnInit() {
 var thisObj = this;
@@ -87,7 +98,7 @@ this.fromDateVal=lastWeekDate;
 this.page(this.offset, this.limit, this.sortvalue, this.sortorder,this.fromDateVal,this.toDateVal);
     var thisObj = this;
  
-}
+         }
 
 FromDate(event){
     this.fromDateVal=event;
@@ -115,20 +126,27 @@ DateRangeForm(){
         @Description: StoryComponent/Task list Rendering
         */
     page(offset, limit, sortvalue, sortorder,fromDateVal,toDateVal ) {
-         this.rows =[];
+       
+
         this._service.getTimeReportDetails(1, offset, limit, sortvalue, sortorder,fromDateVal,toDateVal,(response) => {
            console.log("responseoooo firsttime" +JSON.stringify(response.data))
+
             let jsonForm = {};
             if (response.statusCode == 200) {
+                  this.rows =[];
                 const start = offset * limit;
                 const end = start + limit;
                 let rows = [...this.rows];
                 for (let i = 0; i < response.data.length; i++) {
                     rows[i + start] = response.data[i];
+                    jQuery('.datatable-row-wrapper').addClass('gggg');
                 }
                 this.rows = rows;
+                //alert("rowsssssssss"+JSON.stringify(this.rows));
                 this.count = response.totalCount;
+                //this.ticketdesc=response.tick;
                 this.last7daystimehours=response.timehours;
+
                 var millisecondsPerDay = 1000 * 60 * 60 * 24;
                 var millisBetween = toDateVal.getTime() - fromDateVal.getTime();
                 var days = millisBetween / millisecondsPerDay;
@@ -139,8 +157,6 @@ DateRangeForm(){
                 this.showdays = totalmonth+ " MONTH(S)";}else{
                 var totalYears= fromDateVal.getFullYear() - toDateVal.getFullYear();
                 this.showdays = totalYears+ " YEAR(S)";}
-                
-                
             } else {
                 console.log("fail---");
             }
@@ -149,7 +165,7 @@ DateRangeForm(){
     /*
     @When Clicking pages
     */
-    onPage(event) {alert("-dfd-f-d-");
+    onPage(event) {
         this.offset = event.offset;
         this.limit = event.limit;
         this.page(this.offset, this.limit, this.sortvalue, this.sortorder,this.fromDateVal,this.toDateVal);
@@ -163,14 +179,155 @@ DateRangeForm(){
         this.sortvalue = event.sorts[0].prop;
         this.sortorder = event.sorts[0].dir;
         this.page(this.offset, this.limit, this.sortvalue, this.sortorder,this.fromDateVal,this.toDateVal);
-    }
-  
+}
+
 
     renderStoryForm() {
         this._router.navigate(['story-form']);
     }
+    editTimeReport(){
+          document.getElementById("editTimeReport").style.display='block';
+        //  jQuery('#myModal').on('shown.bs.modal', function () {
+        //     alert("herer");
+        // jQuery('#myInput').focus();
+ 
+        // }) 
+        //  jQuery('#addTimelogModel').on('shown.bs.modal', function () {
+          
+        // jQuery('#myInput').focus();
+ 
+        // }) 
 
-    
 
     }
+    searchTask(event)
+    {
+     var modifiedString=event.query.replace("#","");
+        var post_data={
+        'projectId':1,
+        'sortvalue':'Title',
+       'searchString':modifiedString
+    }
+     let prepareSearchData = [];
+        this._ajaxService.AjaxSubscribe("time-report/get-story-details-for-timelog",post_data,(result)=>
+         { 
+           var subTaskData = result.data;
+            for(let subTaskfield of subTaskData){
+               var currentData = '#'+subTaskfield.TicketId+' '+subTaskfield.Title;
+                 prepareSearchData.push(currentData);
+            }
+           // alert("prepareSearchData"+prepareSearchData);
+           this.search_results=prepareSearchData;
+         });
+    }
 
+
+    getSelectedValueForTask(event)
+    {
+        console.log("gettttttttttt"+event);
+        this.selectedValForTask=event;
+    }
+
+    getSelectedValueForDate(event)
+    {
+      //  alert("ddddddddddddddd");
+        console.log("gettttttttttt@@@@@@@@@@"+event);
+        this.selectedValForDate=event; 
+    }
+
+    addTimeLog(){
+        var getTaskVal=this.text;
+        var date = (this.dateVal.getMonth() + 1) + '-' + this.dateVal.getDate() + '-' +  this.dateVal.getFullYear();
+        date = date.replace(/(\b\d{1}\b)/g, "0$1");
+        var finalDate=this.dateVal.toString();
+        var timelogData={
+              'projectId':1,
+              'addTimelogTask':getTaskVal.split("#")[1],
+              'addTimelogDesc':jQuery('#addTimelogDesc').val(),
+              'addTimelogTime':jQuery('#addTimelogTime').val(),
+              'addTimelogDate':finalDate,
+        }
+       // alert("timelogData"+JSON.stringify(timelogData));
+        this._ajaxService.AjaxSubscribe("time-report/add-timelog",timelogData,(response)=>
+            { 
+              //  console.log("ssssssssssssss first"+JSON.stringify(this.rows));
+                if (response.statusCode == 200) {
+                    this.page(this.offset, this.limit, this.sortvalue, this.sortorder,this.fromDateVal,this.toDateVal);
+               // console.log("onlyyyy" +JSON.stringify(response));
+              // console.log("onlyyyy###" +JSON.stringify(response.data[0]));
+                //  this.rows.push(response.data[0]);
+                //   let rows = [...this.rows];
+                // for (let i = 0; i < response.data.length; i++) {
+                //     rows[i + 0] = response.data[i];
+                // }
+                // this.rows = rows;
+              // console.log("@@@@@@@@@responseoooo final" +JSON.stringify(this.rows));
+            jQuery('.timelogSuccessMsg').css('display','block');
+            jQuery('.timelogSuccessMsg').fadeOut( "slow" );
+                  setTimeout(() => {
+                        jQuery('#addTimelogModel').modal('hide');
+                        jQuery('#addTimelogModel').find("input,p").val('').end();
+              }, 500);
+              
+            } else {
+                console.log("fail---");
+            }
+            });
+    
+    }
+    updateTimelog(slug,ticketDesc,timeLogDate){
+   //  alert(JSON.stringify(ticketDesc));
+     // alert(jQuery('#editTimelogTime'+'_'+slug).val());
+      console.log("eeeeeeee"+this.selectedValForDate);
+      var editableDate = new Date(timeLogDate);
+                var post_data={
+                    'projectId':1,
+                    'slug':slug,
+                    'timelogHours':jQuery('#editTimelogTime'+'_'+slug).val(),
+                    'ticketDesc':ticketDesc,
+                    'description':jQuery('#editableDesc'+'_'+slug).val(),
+                    'autocompleteTask':this.selectedValForTask,
+                    'editableDate':editableDate,
+                    'calendardate':this.selectedValForDate
+                   }
+           this._ajaxService.AjaxSubscribe("time-report/update-timelog-for-edit",post_data,(response)=>
+            { 
+               //alert("onlyyyy" +JSON.stringify(response)); 
+                   if (response.statusCode == 200) {
+                        var input="_Input";
+                      //  alert(slug+input);
+                      //  jQuery('.'+slug+input).replaceWith(response.data[0]);
+                //   this.rows.push(response.data[0]);
+                //   console.log("onlyyyy###" +JSON.stringify(response.data[0]));
+                //   let rows = [...this.rows];
+                // for (let i = 0; i < response.data.length; i++) {
+                //     rows[i + 0] = response.data[i];
+                // }
+                // this.rows = rows;
+                //  console.log("@@@@@@@@@responseoooo final" +JSON.stringify(this.rows));
+            } else {
+                console.log("fail---");
+            }
+            });
+    }
+    showdeleteDiv(){
+        jQuery("#delete_timelog").css("display", "block");
+    }
+    removeTimelog(ticketDesc,slug,timelogHours){
+        var input="_Input";
+      //  alert(slug+input);
+         var postObj={
+                    'projectId':1,
+                    'slug':slug,
+                    'ticketDesc':ticketDesc,
+                    'timelogHours':timelogHours
+                     }
+          this._ajaxService.AjaxSubscribe("time-report/remove-timelog",postObj,(response)=>
+            { 
+              console.log("onlyyyy@@@@@@@@@@@@@@" +JSON.stringify(response)); 
+              //jQuery('.'+slug+input).hide();
+               this.page(this.offset, this.limit, this.sortvalue, this.sortorder,this.fromDateVal,this.toDateVal);
+             // this.page(this.offset, this.limit, this.sortvalue, this.sortorder,this.fromDate,this.toDate);
+            });            
+    }
+}
