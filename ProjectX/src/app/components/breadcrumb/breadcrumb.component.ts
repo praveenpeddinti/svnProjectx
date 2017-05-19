e9921ecc1954fbc87d04fd4ad9e454962eb2a99d
moinhussain <moinhussain@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-05-19 13:26:21 +0000

    fixed node issue
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/branches@1299 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- ProjectX/src/app/components/breadcrumb/breadcrumb.component.ts
+++ ProjectX/src/app/components/breadcrumb/breadcrumb.component.ts
@@ -2,8 +2,10 @@ import {Component,OnInit,Input} from '@angular/core';
 import {ActivatedRoute,Router,NavigationEnd,Params,PRIMARY_OUTLET,UrlTree,UrlSegment,UrlSegmentGroup} from '@angular/router';
 import {MenuItem} from 'primeng/primeng';
 import { GlobalVariable } from '../../config';
+import { AjaxService } from '../../ajax/ajax.service';
 import {SharedService} from '../../services/shared.service'; //this service updates the component variable from another component
-
+declare var io:any;
+declare var socket:any;
 declare var jQuery:any;
 @Component({
   selector: 'breadcrumb',
@@ -12,7 +14,7 @@ declare var jQuery:any;
 })
 
 export class BreadcrumbComponent implements OnInit {
-  
+   public notify_count:any=0;
   private items:any=[];
   private route_url:string;
   private route_params:string;
@@ -22,9 +24,31 @@ export class BreadcrumbComponent implements OnInit {
   private count=0;
   private status=false;
   private isLoggedOut=false;
-  constructor(private router:Router,private route:ActivatedRoute,private shared:SharedService){
+  constructor(private router:Router,private route:ActivatedRoute,private shared:SharedService,private _ajaxService: AjaxService,){
     this.shared=shared;
-    console.log("==in const==");
+    console.log("==in const==BreadcrumbComponent");
+    
+      var thisObj = this;
+    var post_data={}; 
+      thisObj._ajaxService.SocketSubscribe('getAllNotificationsCount',post_data);
+    
+      
+        socket.on('getAllNotificationsCountResponse', function(data) {
+       
+            data = JSON.parse(data); 
+             console.log("getAllNotificationsCountResponse-----------"+data.count);
+            thisObj.notify_count=data.count;
+            if(data.count == 0){
+            jQuery("#notificationCount").hide();
+             jQuery(".notificationlist").remove();
+            }else{
+            jQuery("#notificationCount").show();
+            jQuery("#notificationCount").html(data.count);
+            }
+  
+        });
+    
+    
   }
   ngOnInit()
   {
