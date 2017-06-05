251cf5bc2e5ce2851b187767be553d332aa6081a
padmajad <padmajad@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-06-05 13:55:51 +0000

    search
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/branches@1541 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- ProjectXService/common/config/params-local.php
+++ ProjectXService/common/config/params-local.php
@@ -1,10 +1,13 @@
 <?php
 return [
+
     "ServerURL" => "http://10.10.73.33",
+
     "ProjectRoot" =>"/usr/share/nginx/www/ProjectXService/frontend/web",
     "StoryArtifactPath" => "/files/story",
     "ProjectEmail" => "moin.hussain@techo2.com",
     "AppURL"=> "http://10.10.73.33:4200",
     "ElasticEmailApiKey"=> "9d55f483-0501-4005-8ada-3335f666e731",
-    "EmailServerURL"=>"http://113.193.181.131"
+    "EmailServerURL"=>"http://113.193.181.131",
+    'pageLength'=>10
 ];
