becff1629dd7ba0007d1526147f7617dd7a9e05f
Allamprabhu.Aloji <Allamprabhu.Aloji@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-06-09 13:01:28 +0000

    Code refactored with Moin,cahnged with common methods.
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/branches@1562 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- ProjectXMobile/src/providers/globalservice.ts
+++ ProjectXMobile/src/providers/globalservice.ts
@@ -25,55 +25,43 @@ export class Globalservice {
         this.params.timeZone = "Asia/Kolkata";
         return this.params;
     }
-    public getLoginValidation(url, data) {
-        this.getUserInfo();
-        var response = this.http.post(url, JSON.stringify(data), {headers: this.headers}).map(
+    public ajaxCall(url,data){
+        var userInfo=JSON.parse(localStorage.getItem("userCredentials"));
+        data["userInfo"] = userInfo;
+        data["timeZone"] = "Asia/Kolkata";
+        data["projectId"] = 1;
+          var response = this.http.post(url, JSON.stringify(data), {headers: this.headers}).map(
             res => res.json()
         );
-        return response;
+         return response;
+    }
+    public getLoginValidation (url,data){
+        return this.ajaxCall(url,data);
     }
     public getLogout(url, data) {
-        var response = this.http.post(url, JSON.stringify(data), this.headers).map(
-            res => res.json()
-        );
-        return response;
+           return this.ajaxCall(url,data);
     }
     public getTicketDetailsById(url, data) {
-        var ticketDetailsParams = this.getUserInfo();
+        var ticketDetailsParams = {};
         ticketDetailsParams["ticketId"] = data;
-        var response = this.http.post(url, JSON.stringify(ticketDetailsParams), this.headers).map(
-            res => res.json()
-        );
-        return response;
+        return this.ajaxCall(url,ticketDetailsParams);
     }
     public getStoriesList(url, params) {
-        var response = this.http.post(url,JSON.stringify(params),{headers: this.headers, }).map(
-            res => res.json()
-        );
-        return response;
+        return this.ajaxCall(url,params);
     }
      public getfilterOptions(url, params){
-        var response = this.http.post(url, JSON.stringify(params), {headers: this.headers}).map(
-            res => res.json()
-        );
-        return response;
+         return this.ajaxCall(url,params);
     }
     public getFieldItemById(url, fieldDetails) {
-        var fieldItemParams = this.getUserInfo();
+        var fieldItemParams = {};
         fieldItemParams["fieldId"] = fieldDetails.id;
         fieldItemParams["ticketId"] = fieldDetails.ticketId;
-        fieldItemParams["projectId"] = 1;
-//        fieldItemParams["timeZone"] = "Asia/Kolkata";
         fieldItemParams["workflowType"] = fieldDetails.workflowType;
         fieldItemParams["statusId"] = fieldDetails.readableValue.StateId;
-       
-        var response = this.http.post(url, JSON.stringify(fieldItemParams), this.headers).map(
-            res => res.json()
-        );
-        return response;
+          return this.ajaxCall(url,fieldItemParams);
     }
     public leftFieldUpdateInline(url, selectedItem, fieldDetails){
-        var fieldUpdateParams = this.getUserInfo();
+        var fieldUpdateParams = {};
         fieldUpdateParams["isLeftColumn"] = 1;
         fieldUpdateParams["id"] = fieldDetails.id;
         fieldUpdateParams["value"] = selectedItem;
@@ -84,148 +72,76 @@ export class Globalservice {
             fieldUpdateParams["statusId"] = fieldDetails.readableValue.StateId;
         }
         fieldUpdateParams["projectId"] = 1;
-//        fieldUpdateParams["timeZone"] = "Asia/Kolkata";
         delete fieldUpdateParams["fieldId"];
-      
-       // delete fieldUpdateParams["projectId"];
-        var response = this.http.post(url, JSON.stringify(fieldUpdateParams), this.headers).map(
-            res => res.json()
-        );
-        return response;
+        return this.ajaxCall(url,fieldUpdateParams);
     }
     public newStoryTemplate(url,data){
-        var ticketParams = this.getUserInfo();
-        ticketParams["ticketId"] = data;
-        var response = this.http.post(url, JSON.stringify(ticketParams), this.headers).map(
-            res => res.json()
-        );
-        return response;
+        var ticketParams = {};
+        return this.ajaxCall(url,ticketParams);     
     }
     public createStoryORTask(url, data) {
-        var createStoryParams = this.getUserInfo();
+        var createStoryParams = {};
         createStoryParams["data"] = data;
-//        createStoryParams["timeZone"] = "Asia/Kolkata";
-        var response = this.http.post(url, JSON.stringify(createStoryParams), this.headers).map(
-            res => res.json()
-        );
-        return response;
+        return this.ajaxCall(url,createStoryParams);
     }
     public getTicketActivity(url, activityParams) {
-        var ticketActivityParams = this.getUserInfo();
+         var ticketActivityParams = {};
         ticketActivityParams["ticketId"] = activityParams;
-//        ticketActivityParams["timeZone"] = "Asia/Kolkata";
-        var response = this.http.post(url, JSON.stringify(ticketActivityParams), this.headers).map(
-            res => res.json()
-        );
-        return response;
+         return this.ajaxCall(url,ticketActivityParams);
     }
     public deleteCommentById(url, commentParams){
-        var deleteCommentParams = this.getUserInfo();
+        var deleteCommentParams = {};
         deleteCommentParams["Comment"] = commentParams.Comment;
         deleteCommentParams["ticketId"] = commentParams.TicketId;
-        var response = this.http.post(url, JSON.stringify(deleteCommentParams), this.headers).map(
-            res => res.json()
-        );
-        return response;
+        return this.ajaxCall(url,deleteCommentParams);
     }
     public submitComment(url, commentParams){
-        var submitCommentParams = this.getUserInfo();
+         var submitCommentParams = {};
         submitCommentParams["Comment"] = commentParams.Comment;
         submitCommentParams["ticketId"] = commentParams.TicketId;
-        var response = this.http.post(url, JSON.stringify(submitCommentParams), this.headers).map(
-            res => res.json()
-        );
-        return response;
-    }
-    // Ticket #113
-    //delete it later
-    public getUsersForFollow(url, usersData){
-        var getUsersParams = this.getUserInfo();
-        getUsersParams["ticketId"] = usersData.ticketId;
-        getUsersParams["projectId"] = usersData.ProjectId;
-        getUsersParams["searchValue"] = usersData.SearchValue;
-        var response = this.http.post(url, JSON.stringify(getUsersParams), this.headers).map(
-            res => res.json()
-        );
-        return response;
+        return this.ajaxCall(url,submitCommentParams);
+        
     }
     public makeUsersFollowTicket(url, addFollowerData){
-        var addFollowerParams = this.getUserInfo();
+        var addFollowerParams = {};
         addFollowerParams["ticketId"] = addFollowerData.ticketId;
         addFollowerParams["projectId"] = 1;
         addFollowerParams["collaboratorId"] = addFollowerData.collaboratorId;
-        var response = this.http.post(url, JSON.stringify(addFollowerParams), this.headers).map(
-            res => res.json()
-        );
-        return response;
+         return this.ajaxCall(url,addFollowerParams);
     }
     public makeUsersUnfollowTicket(url, removeFollowerData){
-        var removeFollowerParams = this.getUserInfo();
+         var removeFollowerParams = {};
         removeFollowerParams["ticketId"] = removeFollowerData.ticketId;
         removeFollowerParams["collaboratorId"] = removeFollowerData.collaboratorId;
-        var response = this.http.post(url, JSON.stringify(removeFollowerParams), this.headers).map(
-            res => res.json()
-        );
-        return response;
+        return this.ajaxCall(url,removeFollowerParams);
     }
-    //  Ticket #113 ended
-//sprint 5 start :- prabhu
     public getWorklog(url, worklogParams){
-        var getWorklogParams = this.getUserInfo();
+        var getWorklogParams = {};
         getWorklogParams["ticketId"] = worklogParams;
         getWorklogParams["getimelog"] = worklogParams.getimelog;
-        getWorklogParams["projectId"] = 1;
-       // getWorklogParams["timeZone"] = "Asia/Kolkata";
-        var response = this.http.post(url, JSON.stringify(getWorklogParams), this.headers).map(
-            res => res.json()
-        );
-        return response;
+        return this.ajaxCall(url,getWorklogParams);
     }
     public insertTimelog(url, timelogTicketId, enteredTimeLog){
-        var insertTimelogParams = this.getUserInfo();
+        var insertTimelogParams = {};
         insertTimelogParams["ticketId"] = timelogTicketId;
         insertTimelogParams["workHours"] = enteredTimeLog;
         insertTimelogParams["addTimelogDesc"] = "";
         insertTimelogParams["addTimelogTime"] = this.localDate;
         insertTimelogParams["projectId"] = 1;
-//        insertTimelogParams["timeZone"] = "Asia/Kolkata";
-        var response = this.http.post(url, JSON.stringify(insertTimelogParams), this.headers).map(
-            res => res.json()
-        );
-        return response;
+        insertTimelogParams["timeZone"] = "Asia/Kolkata";
+        return this.ajaxCall(url,insertTimelogParams);
     }
-
     public searchTicket(url, ticketParams){
-        var searchTicketParams = this.getUserInfo();
+        var searchTicketParams = {};
         searchTicketParams["ticketId"] = ticketParams.TicketId;
         searchTicketParams["projectId"] = 1;
         searchTicketParams["sortvalue"] = ticketParams.sortvalue;
         searchTicketParams["searchString"] = ticketParams.searchString;
-//         searchTicketParams["timeZone"] = "Asia/Kolkata";
-         var response = this.http.post(url,JSON.stringify(searchTicketParams),this.headers).map(
-             res => res.json()
-         );
-         return response;
-    }
-
-    public relateTask(url, relatedTask){
-        var relateTaskParams = this.getUserInfo();
-        relateTaskParams["ticketId"] = relatedTask.TicketId;
-        relateTaskParams["projectId"] = 1;
-        relateTaskParams["relatedSearchTicketId"] = relatedTask.relatedSearchTicketId;
-         var response = this.http.post(url,JSON.stringify(relateTaskParams),this.headers).map(
-             res => res.json()
-         );
-         return response;
+        return this.ajaxCall(url,searchTicketParams);
     }
 
    public getCollaborators(url, requestParams) {
-        requestParams['userInfo'] = this.getUserInfo();
-        var response = this.http.post(url, JSON.stringify(requestParams), this.headers).map(
-            res => res.json()
-        );
-        return response;
+        return this.ajaxCall(url,requestParams);
     }
-    //sprint 5 end :- prabhu
     
 }
