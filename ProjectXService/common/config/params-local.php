ab732b6f02b0406987cfbc36f4bb99b6f6034612
praveen.peddinti@techo2.com <praveen.peddinti@techo2.com@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-06-20 09:25:31 +0000

    Pubilc url-- Praveen P
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/branches@1660 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- ProjectXService/common/config/params-local.php
+++ ProjectXService/common/config/params-local.php
@@ -2,7 +2,6 @@
 return [
 
     "ServerURL" => "http://10.10.73.33",
-
     "ProjectRoot" =>"/usr/share/nginx/www/ProjectXService/frontend/web",
     "StoryArtifactPath" => "/files/story",
     "ProjectEmail" => "moin.hussain@techo2.com",
