9574c1aab76f106f69a5db2cafc0107962211a8d
praveen.peddinti@techo2.com <praveen.peddinti@techo2.com@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-06-20 08:48:06 +0000

    public url -- Praveen P
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/branches@1656 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- ProjectXService/common/config/params-local.php
+++ ProjectXService/common/config/params-local.php
@@ -8,6 +8,6 @@ return [
     "ProjectEmail" => "moin.hussain@techo2.com",
     "AppURL"=> "http://10.10.73.33:4200",
     "ElasticEmailApiKey"=> "9d55f483-0501-4005-8ada-3335f666e731",
-    "EmailServerURL"=>"http://113.193.181.131",
+    "EmailServerURL"=>"http://10.10.73.33",
     'pageLength'=>10
 ];
