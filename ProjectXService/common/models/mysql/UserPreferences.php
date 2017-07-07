9e4a7674e5e5ca1c6235f5e62ae29918d662b030
marshal.ryan <marshal.ryan@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-07-07 09:49:30 +0000

    added Model methods for saving and updating User Preferences
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/branches@1719 b02b8b71-e8a6-4a5c-b3be-45d727391186

---
+++ ProjectXService/common/models/mysql/UserPreferences.php
@@ -0,0 +1,85 @@
+<?php 
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace common\models\mysql;
+
+use Yii;
+use yii\base\NotSupportedException;
+use yii\behaviors\TimestampBehavior;
+use yii\db\ActiveRecord;
+use yii\web\IdentityInterface;
+
+class UserPreferences extends ActiveRecord 
+{
+    
+    public static function tableName()
+    {
+        return '{{%UserPreferences}}';
+    }
+    
+    public function behaviors()
+    {
+        return [
+            //TimestampBehavior::className(),
+        ];
+    }
+    
+     /**
+     * @author Ryan Marshal
+     * @param type $userid
+     * @param type $tasks
+     * @return type
+     */
+    public static function savePreference($userid,$tasks)
+    {
+       
+        $prefered_list=$prefered_items='';
+       
+        try
+        {
+             foreach($tasks as $preference_tasks)
+             {
+                  $prefered_list .= $prefered_items  . $preference_tasks->Id ;
+                  $prefered_items = ',';
+             }
+
+             $query = "select * from UserPreferences where CollaboratorId=$userid";
+             $data = Yii::$app->db->createCommand($query)->queryAll();
+             if(sizeof($data)>0) // if there is preference existing
+             {
+                 $query="update UserPreferences set PreferenceItems='$prefered_list' where CollaboratorId=$userid";
+                 $data = Yii::$app->db->createCommand($query)->execute();
+             }
+             else // no preference yet
+             {
+                $preference=new UserPreferences();
+                $preference->CollaboratorId=$userid;
+                $preference->Action="Story Creation";
+                $preference->PreferenceItems=$prefered_list;
+                $preference->save();
+             }
+        } catch (Exception $ex) {
+            Yii::log("UserPreferences:savePreference::" . $ex->getMessage() . "--" . $ex->getTraceAsString(), 'error', 'application');
+        }
+    }
+    
+    /**
+     * @author Ryan Marshal
+     * @param type $userid
+     * @return type
+     */
+    public static function getPreference($userid)
+    {
+        $userid=(int)$userid;
+        try{
+            $query = "select PreferenceItems from UserPreferences where CollaboratorId=3";
+            $data = Yii::$app->db->createCommand($query)->queryOne();
+            return $data;    
+        } catch (Exception $ex) {
+            Yii::log("UserPreferences:getPreference::" . $ex->getMessage() . "--" . $ex->getTraceAsString(), 'error', 'application');
+        }
+    }
+}
