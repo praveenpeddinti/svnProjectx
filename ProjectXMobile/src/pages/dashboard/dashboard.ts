9165f977d688d952bf296102b169650f45688033
anand6396 <anand6396@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-07-07 06:19:24 +0000

    searchbar in dashboard.
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/branches@1711 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- ProjectXMobile/src/pages/dashboard/dashboard.ts
+++ ProjectXMobile/src/pages/dashboard/dashboard.ts
@@ -70,7 +70,7 @@ export class DashboardPage {
         this.headerName = "All My Stories/Task";
         this.SelectValue = "All My Stories/Task";
             this.arrayObject = [];
-            localStorage.setItem('headerInfo',JSON.stringify({'title':"My All Stories",'backButton':"hideBackButton",'logo':1,'leftPannel':0,notification:1,profile:1}));
+            localStorage.setItem('headerInfo',JSON.stringify({'title':"My All Stories",'backButton':"hideBackButton",'logo':1,'leftPannel':0,notification:1,profile:1,searchBar:1}));
 
             this.filterList = [];
             var userInfo=JSON.parse(localStorage.getItem("userCredentials"));
