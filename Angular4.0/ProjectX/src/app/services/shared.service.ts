b4f0bb4c9419798d87c3ee9243cbc7e31a0e346b
marshal.ryan <marshal.ryan@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-07-17 10:34:36 +0000

    added one method to know from where the goToTicket Detail Page is navigated
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/stable@1844 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- Angular4.0/ProjectX/src/app/services/shared.service.ts
+++ Angular4.0/ProjectX/src/app/services/shared.service.ts
@@ -3,7 +3,7 @@ import {Component,Input,Output,Injectable,EventEmitter} from '@angular/core';
 @Injectable()
 export class SharedService {
   @Output() route_change: EventEmitter<any> = new EventEmitter();
-
+            public page;
    constructor() {
      console.log('shared service started');
    }
@@ -11,8 +11,14 @@ export class SharedService {
    change(url,params,page,ticket_type,project) { 
      console.log("==Url in shared service=="+url);
      console.log("==params in shared service=="+params);
-     var route_data={url:url,params:params,page:page,type:ticket_type,projectName:project};
+     var route_data={url:url,params:params,page:page,type:ticket_type,projectName:project,navigatedFrom:this.page};
      this.route_change.emit(route_data);
+     this.page='';
+   }
+
+   navigatedFrom(page)
+   {
+     this.page=page;
    }
 
    getEmittedValue() {
