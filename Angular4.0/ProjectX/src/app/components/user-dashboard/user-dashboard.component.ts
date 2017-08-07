2204725915d75b5fa2b03684fab7fe16352a4a4d
padmajad <padmajad@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-08-07 10:05:45 +0000

    creation project
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/stable@2057 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- Angular4.0/ProjectX/src/app/components/user-dashboard/user-dashboard.component.ts
+++ Angular4.0/ProjectX/src/app/components/user-dashboard/user-dashboard.component.ts
@@ -8,10 +8,15 @@ import { AjaxService } from '../../ajax/ajax.service';
 import {AccordionModule,DropdownModule,SelectItem} from 'primeng/primeng';
 import {SharedService} from '../../services/shared.service';
 import { ProjectService } from '../../services/project.service';
+import { GlobalVariable } from '../../config';
+import { FileUploadService } from '../../services/file-upload.service';
+import {SummerNoteEditorService} from '../../services/summernote-editor.service';
+declare var jQuery:any;
 @Component({
   selector: 'app-user-dashboard',
   templateUrl: './user-dashboard.component.html',
-  styleUrls: ['./user-dashboard.component.css']
+  styleUrls: ['./user-dashboard.component.css'],
+  providers:[AuthGuard,FileUploadService]
 })
 export class UserDashboardComponent implements OnInit {
   public users=JSON.parse(localStorage.getItem('user'));
@@ -22,6 +27,20 @@ export class UserDashboardComponent implements OnInit {
   public dashboardData:any;
   public projects:any;
   public activities:any;
+  public form={};
+  public filesToUpload: Array<File>;
+  public hasBaseDropZoneOver:boolean = false;
+  public hasFileDroped:boolean = false;
+  editorData:string='';
+  public fileUploadStatus:boolean = false;
+  public projectImage:any;
+  public fileExtention:any;
+  public verifyByspinner:any;
+  public summernoteLength=0;
+  public fileuploadClick=false;
+  public verified =0;
+  public submitted=false;
+  public creationPopUp=true;
   constructor(
           private _router: Router,
           private _service: LoginService,
@@ -29,7 +48,9 @@ export class UserDashboardComponent implements OnInit {
           private shared:SharedService,
           private _ajaxService: AjaxService,
           private zone:NgZone,
-  ) { }
+          private fileUploadService: FileUploadService,
+          private editor:SummerNoteEditorService
+  ) {this.filesToUpload = []; }
 
   ngOnInit() {
     var thisObj = this;
@@ -40,12 +61,140 @@ export class UserDashboardComponent implements OnInit {
       activityLimit:this.activityLimit
     }
     thisObj.loadUserDashboard(req_params);
-  }
-
+      setTimeout(() => {
+         var formobj=this;
+        this.editor.initialize_editor('summernote','keyup',formobj);
+         },3000);
+   }
+    ngAfterViewInit() {
+        //  setTimeout(() => {
+        //  var formobj=this;
+        // this.editor.initialize_editor('summernote','keyup',formobj);
+        //  },2000);
+    }
 loadUserDashboard(req_params){
   var thisObj=this;
   this._ajaxService.AjaxSubscribe('collaborator/get-user-dashboard-details',req_params,(result)=>{
    thisObj.dashboardData = result.data;
   });
 }
+  CallFileupload(){
+       jQuery("input[id='my_file']").click(); 
+      this.fileuploadClick=true;
+    }
+        /*
+    @params       : fileInput,comeFrom
+    @ParamType    :  any,string
+    @Description  : Uploading File
+    */
+    public fileUploadEvent(fileInput: any, comeFrom: string):void 
+    {
+       if(comeFrom == 'fileChange') {
+            this.filesToUpload = <Array<File>> fileInput.target.files;
+       } else if(comeFrom == 'fileDrop') {
+            this.filesToUpload = <Array<File>> fileInput.dataTransfer.files;
+       } else {
+            this.filesToUpload = <Array<File>> fileInput.target.files;
+       }
+            
+            this.hasBaseDropZoneOver = false;
+            this.fileUploadStatus = true;
+            this.fileUploadService.makeFileRequest(GlobalVariable.FILE_UPLOAD_URL, [], this.filesToUpload).then(
+                (result :Array<any>) => {
+    
+                for(var i = 0; i<result.length; i++){
+                   result[i].originalname =  result[i].originalname.replace(/[^a-zA-Z0-9.]/g,'_'); 
+                    var uploadedFileExtension = (result[i].originalname).split('.').pop();
+                     if(uploadedFileExtension == "png" || uploadedFileExtension == "jpg" || uploadedFileExtension == "jpeg" || uploadedFileExtension == "gif") {
+                       var postData={
+                              logoName:"[[image:" +result[i].path + "|" + result[i].originalname + "]]"
+                            }
+                          this._ajaxService.AjaxSubscribe("site/get-project-image",postData,(result)=>
+                            {
+                                if(result.data){
+                                    jQuery("#projectlogo").attr("src",result.data);
+                                    this.fileExtention=uploadedFileExtension;
+                               }
+                               this.projectImage=jQuery("#projectlogo").attr("src");
+                            
+                    });
+                     } else{
+                   }
+                }
+                this.fileUploadStatus = false;
+            }, (error) => {
+                console.error("Error occured in story-formcomponent::fileUploadEvent"+error);
+                this.fileUploadStatus = false;
+            });
+    }
+      verifyProjectName(value){
+         
+         var postData={
+                      projectName:value
+                  } ;
+                  if(value=='' || value == undefined){
+                      console.log("sssssssssss");
+                  }else{
+                        this.verifyByspinner=2;
+                       setTimeout(() => {
+                          this._ajaxService.AjaxSubscribe("site/verifying-project-name",postData,(result)=>
+                        { 
+                           
+                            if (result.statusCode == 200) {
+                               this.verified=0;
+                               this.verifyByspinner=3;
+                             }else{
+                              this.verified=1;
+                              this.verifyByspinner=1;
+                            }
+                                    
+                        })
+                         }, 3000);
+                       
+                  }
+    }
+   veryInputByspinner(){
+        this.verifyByspinner='';
+    }
+      saveProjectDetails(){
+        if(this.verified==1){
+
+           this.projectImage=jQuery('#projectlogo').attr("src");
+             var editor=jQuery('#summernote').summernote('code');
+            editor=jQuery(editor).text().trim();
+            if(editor.length>500){
+                this.summernoteLength=1;
+            }else{
+             var postData={
+                      projectName:this.form['projectName'].trim(),
+                      description:this.form['description'].trim(),
+                      projectLogo:this.projectImage,
+                      fileExtention:this.fileExtention
+                  } ;
+              this._ajaxService.AjaxSubscribe("site/save-project-details",postData,(result)=>
+              {
+                    if (result.statusCode == 200) {
+                       setTimeout(() => {
+                          this.submitted=false;
+                          this.form=[];
+                            this.creationPopUp=false;
+                         }, 1000);
+                         this.creationPopUp=true;
+                      }else{
+                 }
+                }) 
+            }
+       }else{
+
+       }
+    }
+ 
+    resetForm(){
+         this.form=[];
+        }
+    creationProject(){
+       this.creationPopUp=true;
+       this.summernoteLength=0;
+       this.verifyByspinner='';
+    }
 }
