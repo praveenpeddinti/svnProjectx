c7f006096641eb23b52dcda2bb3b23810d336cbd
marshal.ryan <marshal.ryan@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-08-10 15:09:03 +0000

    added Invite Url params
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/branches@2090 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- ProjectXService/common/config/params-local.php
+++ ProjectXService/common/config/params-local.php
@@ -5,9 +5,10 @@ return [
     "ProjectRoot" =>"/usr/share/nginx/www/ProjectXService/frontend/web",
     "StoryArtifactPath" => "/files/story",
     "ProjectEmail" => "moin.hussain@techo2.com",
-    "AppURL"=> "http://10.10.73.33:4200",
+    "AppURL"=> "http://10.10.73.33:4200",   
     "ElasticEmailApiKey"=> "9d55f483-0501-4005-8ada-3335f666e731",
     "EmailServerURL"=>"http://10.10.73.33",
-    'pageLength'=>10,
+    "InviteUrl"=>"http://localhost:4200/project/",
+    "pageLength"=>10,
     "projectLogo"=>"/files/projectlogo"
 ];
