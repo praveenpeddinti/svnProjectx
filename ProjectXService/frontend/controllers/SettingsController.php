d22ad5a8e8e6345e751dcb9f87b7102e2277b280
lakshmi.valasapalli <lakshmi.valasapalli@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-08-11 05:46:26 +0000

    notification setting
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/branches@2093 b02b8b71-e8a6-4a5c-b3be-45d727391186

---
+++ ProjectXService/frontend/controllers/SettingsController.php
@@ -0,0 +1,116 @@
+<?php
+namespace frontend\controllers;
+
+use Yii;
+use yii\base\ErrorException;
+use yii\base\InvalidParamException;
+use yii\web\BadRequestHttpException;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+use common\components\CommonUtility;
+use common\models\bean\ResponseBean;
+use common\components\ServiceFactory;
+use common\models\mongo\TinyUserCollection;
+use common\models\mongo\NotificationCollection;
+/**
+ * 
+ * Story Controller
+ */
+class SettingsController extends Controller
+{
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function actions()
+    {
+       return [
+    'components' => [
+        'errorHandler' => [
+            'maxSourceLines' => 20,
+        ],
+    ],
+];
+    }
+
+    
+
+    /**
+     * Logs in a user.
+     *
+     * @return mixed
+     */
+    public function beforeAction($action) {
+    $this->enableCsrfValidation = false;
+    return parent::beforeAction($action);
+    }
+    /**
+     * @author Lakshmi
+     * @params none
+     * @return type
+     */
+    public function actionEmailPreferences(){
+        $details=array();
+        $postData = json_decode(file_get_contents("php://input"));
+        $userId=!empty($postData->userInfo->Id)?$postData->userInfo->Id:"";
+        $details = CommonUtility::getAllNotificationTypes($userId);
+        $responseBean = new ResponseBean();
+         $responseBean->statusCode = ResponseBean::SUCCESS;
+         $responseBean->message = ResponseBean::SUCCESS_MESSAGE;
+         $responseBean->data = $details;
+         $response = CommonUtility::prepareResponse($responseBean,"json");
+         return $response;
+       // return $details;
+    }
+//    public function actionNotificationsStatus(){
+//       $postData = json_decode(file_get_contents("php://input"));
+//        $userId=!empty($postData->userInfo->Id)?$postData->userInfo->Id:"";
+//        $details = CommonUtility::getAllNotificationsStatus($userId);
+//         $responseBean = new ResponseBean();
+//         $responseBean->statusCode = ResponseBean::SUCCESS;
+//         $responseBean->message = ResponseBean::SUCCESS_MESSAGE;
+//         $responseBean->data = $details;
+//         $response = CommonUtility::prepareResponse($responseBean,"json");
+//         return $response;
+//    }
+    public function actionNotificationsSettingsStatusUpdate(){
+        try{
+         $postData = json_decode(file_get_contents("php://input"));
+        $userId=!empty($postData->userInfo->Id)?$postData->userInfo->Id:"";
+        $type=$postData->type;
+        $activityId=$postData->id;
+        $status=$postData->status;
+        $details = CommonUtility::NotificationsSetttingsStatusUpdate($userId,$status,$type,$activityId);
+         $responseBean = new ResponseBean();
+         $responseBean->statusCode = ResponseBean::SUCCESS;
+         $responseBean->message = ResponseBean::SUCCESS_MESSAGE;
+         $responseBean->data = $details;
+         $response = CommonUtility::prepareResponse($responseBean,"json");
+         return $response;
+        }  catch (\Throwable $th)  {
+                Yii::error("SiteController:actionUserAuthentication::" . $th->getMessage() . "--" . $th->getTraceAsString(), 'application');
+             $responseBean = new ResponseBean();
+             $responseBean->statusCode = ResponseBean::SERVER_ERROR_CODE;
+             $responseBean->message = $th->getMessage();
+             $responseBean->data = [];
+             $response = CommonUtility::prepareResponse($responseBean,"json");
+             return $response;
+        }
+      
+    }
+
+
+}
+
+
+?>
