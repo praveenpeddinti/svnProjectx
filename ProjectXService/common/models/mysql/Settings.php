721dd76f53368679ffa5f2eaff58da866a349d1d
lakshmi.valasapalli <lakshmi.valasapalli@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-08-11 05:56:13 +0000

    notification setting
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/branches@2098 b02b8b71-e8a6-4a5c-b3be-45d727391186

---
+++ ProjectXService/common/models/mysql/Settings.php
@@ -0,0 +1,86 @@
+<?php 
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace common\models\mysql;
+
+use Yii;
+use yii\base\NotSupportedException;
+use yii\behaviors\TimestampBehavior;
+use yii\db\ActiveRecord;
+use yii\web\IdentityInterface;
+use yii\base\ErrorException;
+
+class Settings extends ActiveRecord 
+{
+    
+    public static function tableName()
+    {
+        return '{{%Notifications}}';
+    }
+    
+    public function behaviors()
+    {
+        return [
+            //TimestampBehavior::className(),
+        ];
+    }  
+
+     /**
+     * @author Lakshmi
+     * @params $userId
+     * @return $data
+     */
+    public function getAllNotificationTypes($userId){
+        $query="select * from CollaboratorNotificationsSettings cns join
+                Notifications ns on ns.Id=cns.ActivityId where cns.CollaboratorId=$userId";
+        $data = Yii::$app->db->createCommand($query)->queryAll();
+        error_log("+++++++++++++++++++++".sizeof($data));
+        return $data;
+    }
+      /**
+     * @author Lakshmi
+     * @params $userId
+     * @return $data
+     */
+    public function getAllNotificationsStatus($userId){
+//        $query="select * from CollaboratorNotificationsSettings cns join
+//                Notifications ns on ns.Id=cns.ActivityId where CollaboratorId=$userId";
+$query="select * from CollaboratorNotificationsSettings where where CollaboratorId=$userId";                
+       //error_log("++++++++++++++++++++++".$query);     
+        $data = Yii::$app->db->createCommand($query)->queryAll();
+        return $data; 
+    }
+     
+    /**
+     * @author Lakshmi
+     * @params $userId,$status,$type,$activityId
+     * @return $data
+     */
+        public function notificationsSetttingsStatusUpdate($userId,$status,$type,$activityId){
+            if($status==0){
+              $query="update CollaboratorNotificationsSettings set $type=1 where CollaboratorId=$userId and ActivityId=$activityId"; 
+            }else{
+             $query="update CollaboratorNotificationsSettings set $type=0 where CollaboratorId=$userId and ActivityId=$activityId";    
+            }
+        $data = Yii::$app->db->createCommand($query)->execute();
+        return $data; 
+    }
+            public function getNotificationSettingsStatus($fieldName,$userId){
+           $query="select * from Notifications ns join CollaboratorNotificationsSettings cns 
+                    on ns.ActivityOn='$fieldName' and cns.CollaboratorId=$userId and ns.Id=cns.ActivityId"; 
+           error_log("%%%%%%%%%%%%%%%%".$query);
+
+             $data = Yii::$app->db->createCommand($query)->queryAll();
+         //    $status="(".$data[0]['SystemNotification'].','.$data[0]['EmailNotification'].','.$data[0]['PushNotification'].")";
+             return $data; 
+    }
+}
+
+
+            
+            
+
+?>
