718e2d73c93e733a99c00aabcf9b7ce439787fb5
marshal.ryan <marshal.ryan@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-08-14 10:00:02 +0000

    Changed Invite Url to 10.10.73.33
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/branches@2167 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- ProjectXService/common/config/params-local.php
+++ ProjectXService/common/config/params-local.php
@@ -1,14 +1,16 @@
 <?php
 return [
 
-    "ServerURL" => "http://10.10.73.33",
+     "ServerURL" => "http://10.10.73.33",
+    //"ServerURL" => "http://localhost:8050",
     "ProjectRoot" =>"/usr/share/nginx/www/ProjectXService/frontend/web",
     "StoryArtifactPath" => "/files/story",
     "ProjectEmail" => "moin.hussain@techo2.com",
     "AppURL"=> "http://10.10.73.33:4200",   
     "ElasticEmailApiKey"=> "9d55f483-0501-4005-8ada-3335f666e731",
     "EmailServerURL"=>"http://10.10.73.33",
-    "InviteUrl"=>"http://localhost:4200/project/",
+    //"InviteUrl"=>"http://localhost:4200/project/",
+    "InviteUrl"=>"http://10.10.73.33:4200/project/",
     "pageLength"=>10,
     "projectLogo"=>"/files/projectlogo"
 ];
