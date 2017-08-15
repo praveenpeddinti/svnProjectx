f6fa32a5a71788836c421fc5f2d7f5f494bfc6a2
marshal.ryan <marshal.ryan@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-08-15 14:25:23 +0000

    modified Invite Client Logic
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/stable@2197 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- Angular4.0/ProjectX/src/app/components/invite/invite.component.ts
+++ Angular4.0/ProjectX/src/app/components/invite/invite.component.ts
@@ -27,17 +27,6 @@ export class InviteComponent implements OnInit {
       { 
         thisObj.inviteCode=params['code'];
         thisObj.verifyInvitation(thisObj.inviteCode);
-        // thisObj.route.params.subscribe(params => {
-        //       thisObj.projectName=params['projectName'];
-        //       this.projectService.getProjectDetails(thisObj.projectName,(data)=>{ 
-        //           if(data.data!=false){
-        //             thisObj.projectId=data.data.PId;
-        //             console.log(thisObj.projectId);
-        //             thisObj.verifyInvitation(thisObj.inviteCode);
-        //           }
-        //     })
-        //   });
-          
       })
   }
 
@@ -50,41 +39,22 @@ export class InviteComponent implements OnInit {
     { 
       if(result.statusCode==200)
       { 
-        
-        if(result.data.IsValid=='1'){
-           this.projectId=result.data.ProjectId;
-          if(parseInt(result.data.UserId)!=0){  //for existing user...
-            if((userid_from_cookie==id_from_code)){ //auto-login if user exists in cookie....
-              var email_obj={inviteCode:code,email:result.data.Email};
-              this.invalidateInvite(email_obj,id_from_code);
-            }else{
-              this._router.navigate(['login']); // if cookie doesn't exist for user.....
-            }
-          }else{ //for new user.....
-            this._router.navigate(['project',result.data.ProjectName,'create-user'],{queryParams: {email:result.data.Email,code:code}});
+        if(result.data.UserType=='Existing'){
+          if(userid_from_cookie==id_from_code){ 
+            this._router.navigate(['user-dashboard']);
+          }else{
+            this._router.navigate(['login']);
           }
-        }else{
-          this._router.navigate(['login']);
         }
+        else if(result.data.UserType=="New"){
+          this._router.navigate(['project',result.data.PName,'create-user'],{queryParams: {code:code}});
+        }else{
+           this._router.navigate(['login']);
+        }
+
       }
     })   
 
-  }
-
-  invalidateInvite(email_obj,id_from_code){ //make existing user invite invalid and add to team
-
-    this._ajaxService.AjaxSubscribe("collaborator/invalidate-invitation",email_obj,(status)=>
-              {
-                if(status.statusCode==200){
-                    var user_obj={projectId:this.projectId,userid:id_from_code};
-                    this._ajaxService.AjaxSubscribe("collaborator/add-to-team",user_obj,(result)=>{
-                      if(result.statusCode==200){
-                        this._router.navigate(['user-dashboard']);//navigate to User Dashboard....
-                      }
-                    });
-                    
-                }
-              });
   }
 
 }
