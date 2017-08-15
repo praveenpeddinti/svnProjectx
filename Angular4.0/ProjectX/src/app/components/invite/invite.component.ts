420117388c842492f22d65fddf6fd5d15c012fa0
marshal.ryan <marshal.ryan@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-08-15 10:09:39 +0000

    modified invite
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/stable@2189 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- Angular4.0/ProjectX/src/app/components/invite/invite.component.ts
+++ Angular4.0/ProjectX/src/app/components/invite/invite.component.ts
@@ -20,22 +20,23 @@ export class InviteComponent implements OnInit {
   
    }
 
-  ngOnInit() { 
+  ngOnInit() {
       var thisObj=this;
     thisObj.route.queryParams.subscribe(
       params => 
       { 
         thisObj.inviteCode=params['code'];
-        thisObj.route.params.subscribe(params => {
-              thisObj.projectName=params['projectName'];
-              this.projectService.getProjectDetails(thisObj.projectName,(data)=>{ 
-                  if(data.data!=false){
-                    thisObj.projectId=data.data.PId;
-                    console.log(thisObj.projectId);
-                    thisObj.verifyInvitation(thisObj.inviteCode);
-                  }
-            })
-          });
+        thisObj.verifyInvitation(thisObj.inviteCode);
+        // thisObj.route.params.subscribe(params => {
+        //       thisObj.projectName=params['projectName'];
+        //       this.projectService.getProjectDetails(thisObj.projectName,(data)=>{ 
+        //           if(data.data!=false){
+        //             thisObj.projectId=data.data.PId;
+        //             console.log(thisObj.projectId);
+        //             thisObj.verifyInvitation(thisObj.inviteCode);
+        //           }
+        //     })
+        //   });
           
       })
   }
@@ -44,21 +45,23 @@ export class InviteComponent implements OnInit {
   
    var userid_from_cookie=parseInt(this._cookieService.get('user'));
    var id_from_code=parseInt(code.substring(10));
-   var invite_obj={inviteCode:code,projectId:this.projectId};
+   var invite_obj={inviteCode:code};
    this._ajaxService.AjaxSubscribe("collaborator/verify-invitation-code",invite_obj,(result)=>
     { 
       if(result.statusCode==200)
       { 
+        
         if(result.data.IsValid=='1'){
+           this.projectId=result.data.ProjectId;
           if(parseInt(result.data.UserId)!=0){  //for existing user...
             if((userid_from_cookie==id_from_code)){ //auto-login if user exists in cookie....
-              var email_obj={projectId:this.projectId,email:result.data.Email};
+              var email_obj={inviteCode:code,email:result.data.Email};
               this.invalidateInvite(email_obj,id_from_code);
             }else{
               this._router.navigate(['login']); // if cookie doesn't exist for user.....
             }
           }else{ //for new user.....
-            this._router.navigate(['project',this.projectName,'create-user'],{queryParams: {email:result.data.Email}});
+            this._router.navigate(['project',result.data.ProjectName,'create-user'],{queryParams: {email:result.data.Email,code:code}});
           }
         }else{
           this._router.navigate(['login']);
