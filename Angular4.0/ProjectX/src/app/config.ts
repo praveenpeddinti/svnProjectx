6a6d6b73b766da242749cc509b829b7ed063654a
moinhussain <moinhussain@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-08-16 11:52:38 +0000

    create user, invite user code review- refactored code
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/stable@2208 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- Angular4.0/ProjectX/src/app/config.ts
+++ Angular4.0/ProjectX/src/app/config.ts
@@ -2,6 +2,7 @@
 export const GlobalVariable = Object.freeze({
     BASE_API_URL :'http://10.10.73.33/',
     FILE_UPLOAD_URL: 'http://10.10.73.33:4201/upload',
+    IMAGE_UPLOAD_URL: 'http://10.10.73.33:4201/uploadImage',
     NOTIFICATION_URL:'http://10.10.73.33:4201',
 
    // BASE_API_URL :'http://10.10.73.77/',
