a26e39ca6d2b58d9a7ccc2f096f4948c66749732
moinhussain <moinhussain@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-08-22 11:05:31 +0000

    modified logo and styles
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/branches@2291 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- ProjectXService/environments/prod/common/config/params-local.php
+++ ProjectXService/environments/prod/common/config/params-local.php
@@ -1,3 +1,16 @@
 <?php
 return [
+
+     "ServerURL" => "http://113.193.181.131",
+    //"ServerURL" => "http://localhost:8050",
+    "ProjectRoot" =>"/usr/share/nginx/www/ProjectXService/frontend/web",
+    "StoryArtifactPath" => "/files/story",
+    "ProjectEmail" => "moin.hussain@techo2.com",
+    "AppURL"=> "http://113.193.181.131:4200",   
+    "ElasticEmailApiKey"=> "9d55f483-0501-4005-8ada-3335f666e731",
+    "EmailServerURL"=>"http://113.193.181.131",
+    "InviteUrl"=>"http://113.193.181.131:4200",
+    "pageLength"=>10,
+    "projectLogo"=>"/files/projectlogo",
+    "UserProfilePath"=>"/files/user/"
 ];
