584b43a9bd1e6752953873115b2e41d3787681cb
moinhussain <moinhussain@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-08-23 09:52:17 +0000

    added SVNUtility.php
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/branches@2325 b02b8b71-e8a6-4a5c-b3be-45d727391186

---
+++ ProjectXService/common/components/SVNUtility.php
@@ -0,0 +1,66 @@
+<?php
+namespace common\components;
+
+class SVNUtility {
+
+   
+    public static function createRepository($projectName){
+        try{
+            $ch = curl_init();
+            $postfields = array();
+            $postfields['projectName'] = $projectName;
+            error_log("--createRepository----".$projectName);
+            //$postfields['field2'] = urlencode('value2');
+            curl_setopt($ch, CURLOPT_URL, "http://10.10.73.16/test.php");
+            curl_setopt($ch, CURLOPT_USERPWD, "guest:guest");
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $postfields);
+            
+            $result = curl_exec($ch);
+            //$info=curl_exec($ch);
+            //$info = curl_getinfo($ch);
+            error_log("-createRepository response-".print_r($result,1));
+          
+            return $response;
+       } catch (\Throwable $th) {
+            Yii::error("SVNUtility:createRepository::" . $th->getMessage() . "--" . $th->getTraceAsString(), 'application');
+           
+        }
+    }
+    
+    public static function createRepositoryUser($user){
+        try{
+           
+            $userName =  $user->firstName."".$user->lastName;
+            error_log("createRepositoryUser------".$userName);
+            $ch = curl_init();
+            $post_data = array(
+                'username' => $userName,
+                'password' => $user->password
+            );
+            $postfields = array();
+            curl_setopt($ch, CURLOPT_URL, "http://10.10.73.16/user.php");
+            curl_setopt($ch, CURLOPT_USERPWD, "guest:guest");
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+            
+            $result = curl_exec($ch);
+            //$info=curl_exec($ch);
+            //$info = curl_getinfo($ch);
+            error_log("-curl user response-".print_r($result,1));
+            
+        
+            return $response;
+       } catch (\Throwable $th) {
+            Yii::error("SVNUtility:createUser::" . $th->getMessage() . "--" . $th->getTraceAsString(), 'application');
+        }
+    }
+    
+    public static function updateUser(){
+        
+    }
+   
+    
+}
