5cd0fc9ef0089b3874ee45ff67a85c2092ea5ba7
lakshmi.valasapalli <lakshmi.valasapalli@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-08-24 10:08:11 +0000

    Added responsiveness styles and fixed checkbox issues.
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/stable@2353 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- Angular4.0/ProjectX/src/app/components/notification-settings/notification-settings.component.ts
+++ Angular4.0/ProjectX/src/app/components/notification-settings/notification-settings.component.ts
@@ -14,6 +14,7 @@ public SN_count=0;
 public EN_count=0;
 public PN_count=0;
 public NotificationTypesToDisplay=[];
+public totalNotificationsCount=0;
 
   constructor(private _ajaxService: AjaxService,private zone: NgZone) { }
 
@@ -21,17 +22,32 @@ public NotificationTypesToDisplay=[];
 this.notificationPrefernces();
 }
 
+public checkCount(){
+        if(this.totalNotificationsCount==this.SN_count){
+             this.isSelectedAll_SN=true;
+            }
+        if(this.totalNotificationsCount==this.EN_count){
+             this.isSelectedAll_EN=true;
+            }
+        if(this.totalNotificationsCount==this.PN_count){
+             this.isSelectedAll_PN=true;
+            }
+        }
+ 
  /*
   *@Lakshmi
   * Preparing Notification on user preferences
   */
 public notificationPrefernces(){
 var data={};
+    this.SN_count=0;
+    this.EN_count=0;
+    this.PN_count=0;
 this._ajaxService.AjaxSubscribe("settings/notification-preferences",data,(response) => {
     if (response.statusCode == 200) {
     this.zone.run(() =>{ 
     this.NotificationTypesToDisplay= response.data;
-    var totalNotificationsCount = (response.data).length;
+    this.totalNotificationsCount = (response.data).length;
     var data= response.data;
    for(var i=0;data.length>i;i++){
               if(data[i].SystemNotification==1){
@@ -44,38 +60,62 @@ this._ajaxService.AjaxSubscribe("settings/notification-preferences",data,(respon
                this.PN_count=this.PN_count+1;
               }
          }
-
-        if(totalNotificationsCount==this.SN_count){
-             this.isSelectedAll_SN=true;
-            }
-        if(totalNotificationsCount==this.EN_count){
-             this.isSelectedAll_EN=true;
-            }
-        if(totalNotificationsCount==this.PN_count){
-             this.isSelectedAll_PN=true;
-            }
-         })
+        this.checkCount();
+       })
      }
     });
 }
- 
+
 isChecked(Id,type,e){
-  
         var isChecked=e.target.checked;
-       if(isChecked==false){
-        if (type=="SystemNotification")
-          this.isSelectedAll_SN=false;
-         else if(type=="EmailNotification")
-          this.isSelectedAll_EN=false;
-         else
-          this.isSelectedAll_PN=false;
+        if(isChecked==false){
+        if (isChecked==false && type=="SystemNotification"){
+           this.SN_count=this.SN_count-1;
+           this.isSelectedAll_SN=false;
+        }else if(isChecked==false && type=="EmailNotification"){
+            this.EN_count=this.EN_count-1;
+            this.isSelectedAll_EN=false;
+         } else if(isChecked==false && type=="PushNotification"){
+            this.PN_count=this.PN_count-1;
+            this.isSelectedAll_PN=false;
+         }
+        } else{
+            if (type=="SystemNotification"){
+             this.SN_count=this.SN_count+1;         
+         }  else if (type=="EmailNotification"){
+              this.EN_count=this.EN_count+1;               
+          } else if(type=="PushNotification"){
+              this.PN_count=this.PN_count+1;
+         }      
+         this.checkCount();
         }
         var post_data={id:Id,type:type,isChecked:isChecked};
         this._ajaxService.AjaxSubscribe("settings/notifications-settings-status-update",post_data,(response) => {
-     });
+    });
     }
    
 isCheckedAll(type,e){
+    // this.SN_count=0;
+    // this.EN_count=0;
+    // this.PN_count=0;
+      var isChecked=e.target.checked;
+
+       if(isChecked==true){
+           if (type=="SystemNotification")
+           this.SN_count=this.totalNotificationsCount;        
+            else if (type=="EmailNotification")
+            this.EN_count=this.totalNotificationsCount;          
+              else
+               this.PN_count=this.totalNotificationsCount;          
+       }
+       else{
+           if(type=="SystemNotification")
+                 this.SN_count=0;
+           else if(type=="EmailNotification")
+                this.EN_count=0;          
+          else          
+                this.PN_count=0;         
+       }
      var isChecked=e.target.checked; 
     var post_data={isChecked:isChecked,NotificationType:type};
     this._ajaxService.AjaxSubscribe("settings/notifications-settings-status-update-all",post_data,(response) => {
