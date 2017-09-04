9a53cd27f63014357dfec45cba8364cc78e6dc32
padmajad <padmajad@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-09-04 09:28:22 +0000

    issues
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/stable@2457 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- Angular4.0/ProjectX/src/app/components/activities/activities.component.ts
+++ Angular4.0/ProjectX/src/app/components/activities/activities.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router,ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-activities',
   inputs: ['dashboardData','noMoreActivities','noActivitiesFound'],
@@ -6,15 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./activities.component.css']
 })
 export class ActivitiesComponent implements OnInit {
-  public noMoreActivities:boolean = false;
-  public noActivitiesFound:boolean = false;
-  constructor() { 
+  public noMoreActivities:any;
+  public noActivitiesFound:any;
+  constructor(public _router: Router) { 
     
   }
 
   dashboardData:any[];
    ngOnInit() {
-   console.log("@@@@-------"+JSON.stringify(this.dashboardData));
+    console.log("@@@@-------"+JSON.stringify(this.dashboardData));
    
   }
 
