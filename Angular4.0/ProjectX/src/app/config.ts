ff44bd4bd738ed72f2fea67b66103e0c3dd742be
moinhussain <moinhussain@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-09-11 10:15:09 +0000

    git-svn-id: https://subversion.assembla.com/svn/projectx-0/stable@2507 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- Angular4.0/ProjectX/src/app/config.ts
+++ Angular4.0/ProjectX/src/app/config.ts
@@ -1,10 +1,10 @@
 
 export const GlobalVariable = Object.freeze({
 
-    BASE_API_URL :'http://10.10.73.9:8099/',
-    FILE_UPLOAD_URL: 'http://10.10.73.9:4201/upload',
-    IMAGE_UPLOAD_URL: 'http://10.10.73.9:4201/uploadImage',
-    NOTIFICATION_URL:'http://10.10.73.9:4201',
+    BASE_API_URL :'http://10.10.73.33/',
+    FILE_UPLOAD_URL: 'http://10.10.73.33:4201/upload',
+    IMAGE_UPLOAD_URL: 'http://10.10.73.33:4201/uploadImage',
+    NOTIFICATION_URL:'http://10.10.73.33:4201',
 
 
 
