b24b727d94a1245c0bd7f0fff9d5a008146d5e3d
anand6396 <anand6396@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-09-26 13:29:21 +0000

    public ip in index
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/stable@2671 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- Angular4.0/ProjectX/src/app/config.ts
+++ Angular4.0/ProjectX/src/app/config.ts
@@ -1,19 +1,19 @@
 
 export const GlobalVariable = Object.freeze({
 
-//    BASE_API_URL :'http://10.10.73.9:8099/',
-//    FILE_UPLOAD_URL: 'http://10.10.73.9:4201/upload',
-//    IMAGE_UPLOAD_URL: 'http://10.10.73.9:4201/uploadImage',
-//    NOTIFICATION_URL:'http://10.10.73.9:4201',
+    // BASE_API_URL :'http://10.10.73.9:8099/',
+    // FILE_UPLOAD_URL: 'http://10.10.73.9:4201/upload',
+    // IMAGE_UPLOAD_URL: 'http://10.10.73.9:4201/uploadImage',
+    // NOTIFICATION_URL:'http://10.10.73.9:4201',
 //
 
 
 
 
-    BASE_API_URL :'http://10.10.73.77/',
-    FILE_UPLOAD_URL: 'http://10.10.73.77:4201/upload',
-   IMAGE_UPLOAD_URL: 'http://10.10.73.77:4201/uploadImage',
-    NOTIFICATION_URL:'http://10.10.73.77:4201',
+   BASE_API_URL :'http://10.10.73.77/',
+   FILE_UPLOAD_URL: 'http://10.10.73.77:4201/upload',
+  IMAGE_UPLOAD_URL: 'http://10.10.73.77:4201/uploadImage',
+   NOTIFICATION_URL:'http://10.10.73.77:4201',
 
      //... more of your  variables
  });
