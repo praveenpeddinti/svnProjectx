b788142df251ea9fac0dc70dbef3e036567307c3
anand6396 <anand6396@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-12-08 06:19:26 +0000

    Issue fix: @mention Auto suggestions Issue, Now this is not case sensitive.
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/stable@2718 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- Angular4.0/ProjectX/src/app/services/summernote-editor.service.ts
+++ Angular4.0/ProjectX/src/app/services/summernote-editor.service.ts
@@ -10,65 +10,67 @@ declare var jstz:any;
  * @description: This is used for initializing the summernote editor with @mentions
  * @param: element,options
  */
-@Injectable()
-export class SummerNoteEditorService
-{
-   
-    mention:any=[];
-    constructor(@Inject(AjaxService) this_obj:AjaxService,private sharedService:SharedService)
-    {
-    }
+ @Injectable()
+ export class SummerNoteEditorService
+ {
 
-    initialize_editor(element,options,obj)
-    {
-        var mention_data;
-        var thisObj= this;
-        if(options=='keyup' && obj!=null)
-        {
-            jQuery('#'+element).summernote(
-            {
-                height:200,
-                toolbar: [
-                                ['style', ['bold', 'italic', 'underline', 'clear']],
-                                ['para', ['ul', 'ol']],
-                                ['link', ['linkDialogShow', 'unlink']]
-                         ],
-                hint: {
-                            mentions:[],
-                            match: /\B@(\w*)$/,
-                            users:function(keyword,callback)
-                            {
-                                var params={search_term:keyword};
-                                var getAllData=  JSON.parse(localStorage.getItem('user'));
-                                if(getAllData != null){
-                                    params["userInfo"] = getAllData;
+     mention:any=[];
+     constructor(@Inject(AjaxService) this_obj:AjaxService,private sharedService:SharedService)
+     {
+     }
+
+     initialize_editor(element,options,obj)
+     {
+         var mention_data;
+         var thisObj= this;
+         if(options=='keyup' && obj!=null)
+         {
+             jQuery('#'+element).summernote(
+             {
+                 height:200,
+                 toolbar: [
+                 ['style', ['bold', 'italic', 'underline', 'clear']],
+                 ['para', ['ul', 'ol']],
+                 ['link', ['linkDialogShow', 'unlink']]
+                 ],
+                 hint: {
+                     mentions:[],
+                     match: /\B@(\w*)$/,
+                     users:function(keyword,callback)
+                     {
+                         var params={search_term:keyword};
+                         var getAllData=  JSON.parse(localStorage.getItem('user'));
+                         if(getAllData != null){
+                             params["userInfo"] = getAllData;
                                     params["projectId"] = obj.projectId;//parseInt(localStorage.getItem('ProjectId'));
                                     params["timeZone"] = jstz.determine_timezone().name();
-                                    }
+                                }
                                 var url=GlobalVariable.BASE_API_URL+"story/get-collaborators";
                                 jQuery.ajax(
+                                {
+                                    url:url,
+                                    type:'POST',
+                                    data:JSON.stringify(params),
+                                    async:true
+                                }).done(function(data)
+                                {
+                                    if(data.statusCode!=200){
+                                        thisObj.sharedService.setToasterValue(data.message);
+                                    }
+                                    var mention_list=[];
+                                    for(let i in data.data)
                                     {
-                                        url:url,
-                                        type:'POST',
-                                        data:JSON.stringify(params),
-                                        async:true
-                                    }).done(function(data)
-                                        {
-                                            if(data.statusCode!=200){
-                                            thisObj.sharedService.setToasterValue(data.message);
-                                            }
-                                            var mention_list=[];
-                                            for(let i in data.data)
-                                            {
-                                              mention_list.push({'name':data.data[i].Name,'Profile':data.data[i].ProfilePic});
-                                            }
+                                        mention_list.push({'name':data.data[i].Name,'Profile':data.data[i].ProfilePic});
+                                    }
                                            
-                                            this.mentions=mention_list;
-                                            callback(jQuery.grep(this.mentions, function (item) {
-                                            return item.name.indexOf(keyword) == 0;
-                                             }));
+                                    this.mentions=mention_list;
+                                    callback(jQuery.grep(this.mentions, function (item) {
+                                        // console.log(item.name.toLowerCase().indexOf(keyword.toLowerCase()));
+                                        // return item.name.indexOf(keyword) == 0; //old-one
+                                        return item.name.toLowerCase().indexOf(keyword.toLowerCase()) == 0; //Edited@waheed to avoid case sensitive in autocomplete. 
+                                    }));
 
-                                        });
+                                });
                                 
                                 
                             },        
@@ -78,95 +80,95 @@ export class SummerNoteEditorService
                                     this.users(keyword,callback);
 
                                 }
-                            
+
                             },
                             template: function (item) {
-                               return '<div value="'+item.name + '" name="'+item.name+ '"><img width="20" height="20" src="' + item.Profile + '"/>&nbsp;'+item.name+'</div>';
+                                return '<div value="'+item.name + '" name="'+item.name+ '"><img width="20" height="20" src="' + item.Profile + '"/>&nbsp;'+item.name+'</div>';
                             },
                             content: function (item) {
-                                    return '@' + item.name;
-                                    
-                                    }    
+                                return '@' + item.name;
+
+                            }    
                         },
-                    callbacks: {
-                    onKeyup: function(e) {
-                         this.summernoteLength=false; 
-                       var editor=jQuery('#'+element).summernote('code');
+                        callbacks: {
+                            onKeyup: function(e) {
+                                this.summernoteLength=false; 
+                                var editor=jQuery('#'+element).summernote('code');
                          if(!(editor.indexOf("<p>")>-1)){ //added for default unexpected behaviour of editor
-                              editor="<p>"+editor+"</p>";
-                          }
-                        editor = editor.replace(/^(<p>(<br>)*\s*(<br>)*<\/p>)*(<br>)*|(<p>(<br>)*\s*(<br>)*<\/p>)*(<br>)*$/gi, "");
+                             editor="<p>"+editor+"</p>";
+                         }
+                         editor = editor.replace(/^(<p>(<br>)*\s*(<br>)*<\/p>)*(<br>)*|(<p>(<br>)*\s*(<br>)*<\/p>)*(<br>)*$/gi, "");
                          if(obj.formB != undefined){
                              obj.formB['description']=editor;
                          }
                          if(element=="summernote"){ // added to Fix Padmaja's Issues/Conflicts
-                         editor=jQuery(editor).text().trim();
+                             editor=jQuery(editor).text().trim();
+                     }
+                     if(obj.form != undefined){
+                         obj.form['description']=editor;
+                         if(editor!='')
+                         {
+                             obj.form['description']=editor;
                          }
-                         if(obj.form != undefined){
-                            obj.form['description']=editor;
-                            if(editor!='')
-                            {
-                                obj.form['description']=editor;
-                            }
-                            else
-                            {
-                                obj.form['description']='';
-                            }
+                         else
+                         {
+                             obj.form['description']='';
                          }
+                     }
 
-                    }},
-                    disableDragAndDrop:true,
+                 }},
+                 disableDragAndDrop:true,
 
-            }
-            );
-        }
-        else
-        {
-            jQuery("#"+element).summernote(
-                {
-                    height:200,
-                    toolbar: [
-                                ['style', ['bold', 'italic', 'underline', 'clear']],
-                                ['para', ['ul', 'ol']],
-                                ['link', ['linkDialogShow', 'unlink']]
-                            ],                   
+             }
+             );
+}
+else
+{
+    jQuery("#"+element).summernote(
+    {
+        height:200,
+        toolbar: [
+        ['style', ['bold', 'italic', 'underline', 'clear']],
+        ['para', ['ul', 'ol']],
+        ['link', ['linkDialogShow', 'unlink']]
+        ],                   
 
-                    hint: {
-                            mentions:[],
-                            match: /\B@(\w*)$/,
-                            users:function(keyword,callback)
-                            {
-                                var params={search_term:keyword};
-                                var getAllData=  JSON.parse(localStorage.getItem('user'));
-                                if(getAllData != null){
-                                    params["userInfo"] = getAllData;
+        hint: {
+            mentions:[],
+            match: /\B@(\w*)$/,
+            users:function(keyword,callback)
+            {
+                var params={search_term:keyword};
+                var getAllData=  JSON.parse(localStorage.getItem('user'));
+                if(getAllData != null){
+                    params["userInfo"] = getAllData;
                                     params["projectId"] = obj.projectId;//localStorage.getItem('ProjectId');
                                     params["timeZone"] = jstz.determine_timezone().name();
-                                    }
+                                }
                                 var url=GlobalVariable.BASE_API_URL+"story/get-collaborators";
                                 jQuery.ajax(
+                                {
+                                    url:url,
+                                    type:'POST',
+                                    data:JSON.stringify(params),
+                                    async:true
+                                }).done(function(data)
+                                {
+                                    if(data.statusCode!=200){
+                                        thisObj.sharedService.setToasterValue(data.message);
+                                    }
+                                    var mention_list=[];
+                                    for(let i in data.data)
                                     {
-                                        url:url,
-                                        type:'POST',
-                                        data:JSON.stringify(params),
-                                        async:true
-                                    }).done(function(data)
-                                        {
-                                             if(data.statusCode!=200){
-                                            thisObj.sharedService.setToasterValue(data.message);
-                                            }
-                                            var mention_list=[];
-                                            for(let i in data.data)
-                                            {
-                                              mention_list.push({'name':data.data[i].Name,'Profile':data.data[i].ProfilePic});
-                                            }
-                                           
-                                            this.mentions=mention_list;
-                                            callback(jQuery.grep(this.mentions, function (item) {
-                                            return item.name.indexOf(keyword) == 0;
-                                             }));
+                                        mention_list.push({'name':data.data[i].Name,'Profile':data.data[i].ProfilePic});
+                                    }
 
-                                        });
+                                    this.mentions=mention_list;
+                                    callback(jQuery.grep(this.mentions, function (item) {
+                                        return item.name.indexOf(keyword) == 0;
+                                    }));
+
+                                });
                                 
                                 
                             },        
@@ -176,21 +178,21 @@ export class SummerNoteEditorService
                                     this.users(keyword,callback);
 
                                 }
-                            
+
                             },
                             template: function (item) {
-                               return '<div value="'+item.name + '" name="'+item.name+ '"><img width="20" height="20" src="' + item.Profile + '"/>&nbsp;'+item.name+'</div>';
+                                return '<div value="'+item.name + '" name="'+item.name+ '"><img width="20" height="20" src="' + item.Profile + '"/>&nbsp;'+item.name+'</div>';
                             },
                             content: function (item) {
-                                    return '@' + item.name;
-                                     
-                                    }    
+                                return '@' + item.name;
+
+                            }    
                         },
-                    disableDragAndDrop:true,
-                    
-                }
-            );
-        }
-    }
+                        disableDragAndDrop:true,
+
+                    }
+                    );
+}
+}
 }
 
