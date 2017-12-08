7c1fce5804d69e81f6ca5954971f570b51990f93
anand6396 <anand6396@b02b8b71-e8a6-4a5c-b3be-45d727391186>
2017-12-08 06:15:36 +0000

    Issue fix: Number of notification does not appear immediately in notification badging
    git-svn-id: https://subversion.assembla.com/svn/projectx-0/stable@2717 b02b8b71-e8a6-4a5c-b3be-45d727391186

--- Angular4.0/ProjectX/src/app/header/header.component.ts
+++ Angular4.0/ProjectX/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,AfterViewChecked,OnDestroy,Input} from '@angular/core';
+import { Component, OnInit,AfterViewChecked,OnDestroy,Input, NgZone} from '@angular/core'; // <== ngzone added for notification update @Waheed
 import { Router,ActivatedRoute} from '@angular/router';
 import { Headers, Http } from '@angular/http';
 import { LoginService, Collaborator } from '../services/login.service';
@@ -20,12 +20,12 @@ declare var jQuery:any;
 export class HeaderComponent implements OnInit {
 
   public toasterconfig : ToasterConfig = 
-        new ToasterConfig({
-            showCloseButton: false, 
-            tapToDismiss: false, 
-            timeout: 5000,
-            positionClass: "toast-bottom-right",
-        }); 
+  new ToasterConfig({
+    showCloseButton: false, 
+    tapToDismiss: false, 
+    timeout: 5000,
+    positionClass: "toast-bottom-right",
+  }); 
   public users=JSON.parse(localStorage.getItem('user'));
   public profilePicture=localStorage.getItem('profilePicture');
   public ProjectName=localStorage.getItem('ProjectName');
@@ -44,90 +44,94 @@ export class HeaderComponent implements OnInit {
   private projectId;
   public projectName;
   public srchStr:any;
-   constructor(
+  constructor(
     private _ajaxService: AjaxService,
     public _router: Router,
     private http: Http,
     private _service: LoginService,
     private shared:SharedService,
     private toasterService: ToasterService,
-    private projectService:ProjectService,private route:ActivatedRoute
-       ) { }
+    private projectService:ProjectService,
+    private zone:NgZone,// <== added for notification update @Waheed
+    private route:ActivatedRoute
+    ) { }
 
   ngOnInit() {
 
-     if(this._router.url=='/home'){
+    if(this._router.url=='/home'){
       this.ProjectName='';// to remove right side sticky menu.
     }else{
-  var thisObj = this;
-  thisObj.route.queryParams.subscribe(
-      params => 
-      { 
-      thisObj.route.params.subscribe(params => {
-           thisObj.projectName=params['projectName'];
-           if(typeof thisObj.projectName != "undefined"){
-             thisObj.projectService.getProjectDetails(thisObj.projectName,(data)=>{
+      var thisObj = this;
+      thisObj.route.queryParams.subscribe(
+        params => 
+        { 
+          thisObj.route.params.subscribe(params => {
+            thisObj.projectName=params['projectName'];
+            if(typeof thisObj.projectName != "undefined"){
+              thisObj.projectService.getProjectDetails(thisObj.projectName,(data)=>{
                 if(data.statusCode ==200) {
-                thisObj.projectId=data.data.PId; 
-               }
+                  thisObj.projectId=data.data.PId; 
+                }
                 
+              });
+            }
+
+          });
         });
-           }
-           
-        });
-           });
     }
     
- var thisObj = this;
- this.shared.getNotificationCount().subscribe(value=>
-      { 
-        if(value == -1){ //decement by 1
+    var thisObj = this;
+    this.shared.getNotificationCount().subscribe(value=>
+    { 
+        if(value == -1){ //decrement by 1
           thisObj.notify_count = thisObj.notify_count - 1;
         }else{
           thisObj.notify_count = value;
         }
-       
+
         thisObj.notification_msg = [];
       });
 
- this.shared.getToasterValue().subscribe(value=>
-      {
-        if(thisObj.toast)
-      thisObj.toasterService.clear(thisObj.toast.toastId, thisObj.toast.toastContainerId); 
+    this.shared.getToasterValue().subscribe(value=>
+    {
+      if(thisObj.toast)
+        thisObj.toasterService.clear(thisObj.toast.toastId, thisObj.toast.toastContainerId); 
       thisObj.toast = thisObj.toasterService.pop('error','Server Error!!!',value);
       
-      });
-       this.shared.getLoader().subscribe(value=>
-      { 
-       thisObj.showLoader=value;
-      });
-  /* For Notifications */
-  this.PName=localStorage.getItem('ProjectName');
+    });
+    this.shared.getLoader().subscribe(value=>
+    { 
+      thisObj.showLoader=value;
+    });
+    /* For Notifications */
+    this.PName=localStorage.getItem('ProjectName');
     if(this.users)
     {
-     var thisObj = this;
-    var post_data={}; 
+      var thisObj = this;
+      var post_data={}; 
       thisObj._ajaxService.SocketSubscribe('getAllNotificationsCount',post_data);
-          socket.on('getAllNotificationsCountResponse', function(data) {
-       
-            data = JSON.parse(data); 
-             console.log("getAllNotificationsCountResponse-----------"+data.count);
-            thisObj.notify_count=data.count;
-            if(data.count == 0){
-                 thisObj.notification_msg = [];
-            }
-  
+      socket.on('getAllNotificationsCountResponse', function(data) {
+
+        data = JSON.parse(data); 
+        console.log("--getAllNotificationsCountResponse--"+data.count);
+        thisObj.zone.run(() => { // <== added for notification update @Waheed
+          thisObj.notify_count=data.count;
         });
+        if(data.count == 0){
+          thisObj.notification_msg = [];
+        }
+
+      });
 
     }
     
-     jQuery(document).ready(function(){
+    jQuery(document).ready(function(){
       jQuery(document).bind("click",function(event){   
-          if(jQuery(event.target).closest('ul#notifications_list').length == 0 && jQuery(event.target).closest('li#notificationIcon').length == 0){
-           jQuery("#notifications_list").hide();
-          }
+        if(jQuery(event.target).closest('ul#notifications_list').length == 0 && jQuery(event.target).closest('li#notificationIcon').length == 0){
+          jQuery("#notifications_list").hide();
+        }
       });
-     
+
     });
 
     // Following Code is the Fix for Editor Mention Hint Popover that was not disappearing before
@@ -137,60 +141,60 @@ export class HeaderComponent implements OnInit {
       jQuery("div.note-popover.popover.in.note-hint-popover").css("display","none");
     }
     
-  
-      }
 
-      ngAfterViewChecked(){
-      }
-     ngOnDestroy(){
-        var thisObj = this;
-        socket.off('getAllNotificationsCountResponse');
-      }
+  }
+
+  ngAfterViewChecked(){
+  }
+  ngOnDestroy(){
+    var thisObj = this;
+    socket.off('getAllNotificationsCountResponse');
+  }
   
-     
+
   logout() {
-         this._service.logout((data)=>{ 
-              this._router.navigate(['login']);
-              this.shared.change(null,null,'Logout',null,''); 
-        });
+    this._service.logout((data)=>{ 
+      this._router.navigate(['login']);
+      this.shared.change(null,null,'Logout',null,''); 
+    });
 
-    }
-    NotificationSettings(){
-     this._router.navigate(['NotificationSettings']);
-    }
-projectsArray(list){
-  var listItem=[];
- listItem.push({label:"Projects", value:''});
+  }
+  NotificationSettings(){
+    this._router.navigate(['NotificationSettings']);
+  }
+  projectsArray(list){
+    var listItem=[];
+    listItem.push({label:"Projects", value:''});
     var listMainArray=[];
-     if(list.length>0){
-         for(var i=0;list.length>i;i++){
-           listItem.push({label:list[i].ProjectName, value:{'id':list[i].PId,'name':list[i].ProjectName}});
-         } 
+    if(list.length>0){
+      for(var i=0;list.length>i;i++){
+        listItem.push({label:list[i].ProjectName, value:{'id':list[i].PId,'name':list[i].ProjectName}});
+      } 
     }     
-      listMainArray.push({type:"",filterValue:listItem});
-      return listMainArray;
-}
+    listMainArray.push({type:"",filterValue:listItem});
+    return listMainArray;
+  }
   globalSearchNavigate(){
-  var searchString=this.srchStr;
-     if(this._router.url=='/home'){
+    var searchString=this.srchStr;
+    if(this._router.url=='/home'){
       delete this.PName;
-    
+
     }
     if(typeof searchString !== 'undefined'){
-          if(this.projectName=='' || this.projectName==undefined){
-              this._router.navigate(['search',],{queryParams: {q:searchString}});
-          }else{
-             this._router.navigate(['project',this.projectName,'search'],{queryParams: {q:searchString}});
-       }
+      if(this.projectName=='' || this.projectName==undefined){
+        this._router.navigate(['search',],{queryParams: {q:searchString}});
+      }else{
+        this._router.navigate(['project',this.projectName,'search'],{queryParams: {q:searchString}});
+      }
       
     }else{
-     console.log("here only");
+      console.log("here only");
     }
-    }
+  }
   showErrorFunction(id,message){
-          jQuery("#"+id).html(message);
-          jQuery("#"+id).show();
-          jQuery("#"+id).fadeOut(4000);
+    jQuery("#"+id).html(message);
+    jQuery("#"+id).show();
+    jQuery("#"+id).fadeOut(4000);
   }
 
   readNotification(project,notify_id,event,domIndex)
@@ -200,15 +204,15 @@ projectsArray(list){
     var post_data={'projectId':project.PId,'notifyid':notify_id,viewAll:0,page:this.pageNo};
     this._ajaxService.AjaxSubscribe('story/read-notification',post_data,(data)=>
     {
-    this.notification_msg=[];
+      this.notification_msg=[];
       if(data)
       {
         this.notify_count = data.totalCount;
-      if(data.data != "nodata"){
-        this.notification_msg = data.data;
-      
-      }
-       
+        if(data.data != "nodata"){
+          this.notification_msg = data.data;
+
+        }
+
       }
     })
 
@@ -222,7 +226,7 @@ projectsArray(list){
     {
       if(data)
       {
-         this.notify_count = data.totalCount;
+        this.notify_count = data.totalCount;
         
       }
       this._router.navigate(['project',project.ProjectName,ticketid,'details'],{queryParams: {Slug:slug,From:"Notification"}});
@@ -236,7 +240,7 @@ projectsArray(list){
     {
       if(data)
       {
-          this.notify_count = data.totalCount;
+        this.notify_count = data.totalCount;
         
       }
     })
@@ -251,37 +255,37 @@ projectsArray(list){
       {
         this.notify_count=0;
         this.notification_msg=[];
-       
+
       }
     })
   }
 
   showNotifications()
   {
-  if(jQuery("#notifications_list").is(":visible")){
-    jQuery("#notifications_list").hide();
-   }else{
-     jQuery("#notifications_list").show();
-   if(this.notify_count>0){
-    console.log("show notify");
-  var post_data={viewAll:0,page:1};
-  if(this.getnotificationTimeout != 0){
-   clearTimeout(this.getnotificationTimeout);
- }
- 
- this.getnotificationTimeout = setTimeout(()=>{
-      this._ajaxService.NodeSubscribe('/getAllNotifications',post_data,(data)=>
-      {
-       this.notification_msg=[];
-      if(data.notify_result.length >0){
-      this.notification_msg = data.notify_result;
-     
+    if(jQuery("#notifications_list").is(":visible")){
+      jQuery("#notifications_list").hide();
+    }else{
+      jQuery("#notifications_list").show();
+      if(this.notify_count>0){
+        console.log("show notify");
+        var post_data={viewAll:0,page:1};
+        if(this.getnotificationTimeout != 0){
+          clearTimeout(this.getnotificationTimeout);
         }
-     
-      });
-},100);
-  
-    }
+
+        this.getnotificationTimeout = setTimeout(()=>{
+          this._ajaxService.NodeSubscribe('/getAllNotifications',post_data,(data)=>
+          {
+            this.notification_msg=[];
+            if(data.notify_result.length >0){
+              this.notification_msg = data.notify_result;
+
+            }
+
+          });
+        },100);
+
+      }
 
     }
   }
@@ -297,17 +301,17 @@ projectsArray(list){
   }
   
   TimeReport(){
-  this._router.navigate(['project',this.projectName,'time-report']);
-}
+    this._router.navigate(['project',this.projectName,'time-report']);
+  }
 
- changeProject(){
-        localStorage.setItem('ProjectName',this.selectedProject.name);
-        localStorage.setItem('ProjectId',this.selectedProject.id);
-        this._router.navigate(['project',this.selectedProject.name,'list']);
-    }
+  changeProject(){
+    localStorage.setItem('ProjectName',this.selectedProject.name);
+    localStorage.setItem('ProjectId',this.selectedProject.id);
+    this._router.navigate(['project',this.selectedProject.name,'list']);
+  }
 
-Bucket(){
-  this._router.navigate(['project',this.projectName,'bucket']);
-}
+  Bucket(){
+    this._router.navigate(['project',this.projectName,'bucket']);
+  }
 
 }
